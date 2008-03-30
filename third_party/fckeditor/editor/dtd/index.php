e4adfc4e80b86a19e885f526058fbf781f942dab
luciash <luciash@b456876b-0849-0410-b77d-98878d47e9d5>
2008-03-30 20:06:05 +0000

    [MOD] svn:keywords setting - adding property for keyword Id ($Id: ... $) expansion -- $Header: $ CVS keyword is obsolete, use $Id: $ instead

--- third_party/fckeditor/editor/dtd/index.php
+++ third_party/fckeditor/editor/dtd/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/_fckeditor/editor/dtd/index.php,v 1.1.2.1 2007-12-01 18:28:09 nyloth Exp $
+// $Id: /cvsroot/tikiwiki/_fckeditor/editor/dtd/index.php,v 1.1.2.1 2007-12-01 18:28:09 nyloth Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
