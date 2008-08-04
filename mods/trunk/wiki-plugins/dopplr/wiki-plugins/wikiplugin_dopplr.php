afe21673eb06b896b4565db379a15811ba4c879e
franck <franck@b456876b-0849-0410-b77d-98878d47e9d5>
2008-08-04 03:28:59 +0000

    [ENH] allows more than one dopplr badge on a wiki page

--- mods/trunk/wiki-plugins/dopplr/wiki-plugins/wikiplugin_dopplr.php
+++ mods/trunk/wiki-plugins/dopplr/wiki-plugins/wikiplugin_dopplr.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/dopplr/wiki-plugins/wikiplugin_dopplr.php,v 1.1 2007-12-20 23:37:10 franck Exp $
+ * $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/dopplr/wiki-plugins/wikiplugin_dopplr.php,v 1.2 2008/06/09 01:06:24 franck Exp $
  *
  * Dopplr plugin. Add a Dopplr badge 
  * 
@@ -20,7 +20,7 @@ function wikiplugin_dopplr($data, $params) {
 	extract ($params,EXTR_SKIP);
 	$badgeid    = (isset($badgeid) && ctype_xdigit($badgeid)) ? "$badgeid"  : "";
 
-	$begin  = '<div id="dopplr-blog-badge"><script src="http://www.dopplr.com/blogbadge/script/'.$badgeid.'"></script></div>';
+	$begin  = '<div id="dopplr-blog-badge-for-'.$badgeid.'"><script src="http://www.dopplr.com/blogbadge/script/'.$badgeid.'?div-id=dopplr-blog-badge-for-'.$badgeid.'"></script></div>';
 	$data = "";
 	$end = "";
 	return $begin . $data . $end;
