69b08e02818a8548565e605f799613dfc02a81ae
luciash <luciash@b456876b-0849-0410-b77d-98878d47e9d5>
2009-03-27 01:32:11 +0000

    [FIX] flowplayer: some fixes and corrections to make it work not only in the root of a site but also when having tiki installed in a subdir, fixed autoplay param

--- mods/trunk/wiki-plugins/flowplayer/wiki-plugins/wikiplugin_flowplayer.php
+++ mods/trunk/wiki-plugins/flowplayer/wiki-plugins/wikiplugin_flowplayer.php
@@ -8,7 +8,7 @@ function wikiplugin_flowplayer($data, $params) {
 
 	extract ($params,EXTR_SKIP);
 
-	if ((!isset($videofile)) && (!isset($configfile))) return "Specify videofile or configfile parameter to use and setup flowplayer";
+	if ((!isset($videofile)) && (!isset($configfile))) return "Specify either videofile or configfile parameter to use and setup flowplayer";
 
 	if (!isset($title)) $title='FlowPlayer Video';
 	if (!isset($autoplay)) $autoplay='true';
@@ -18,28 +18,35 @@ function wikiplugin_flowplayer($data, $params) {
 	if (!isset($height)) $height=263;
 
 	if ((isset($player)) && ($player=='longplay')) {
-	if (!isset($engine)) $engine='/lib/flowplayer/FlowPlayerLP.swf';
+	if (!isset($engine)) $engine='lib/flowplayer/FlowPlayerLP.swf';
 	} elseif ((isset($player)) && ($player=='skinnable')) {
-		if (!isset($engine)) $engine='/lib/flowplayer/FlowPlayerLight.swf';
+		if (!isset($engine)) $engine='lib/flowplayer/FlowPlayerLight.swf';
 	} elseif ((isset($player)) && ($player=='dark')) {
-		if (!isset($engine)) $engine='/lib/flowplayer/FlowPlayerDark.swf';
+		if (!isset($engine)) $engine='lib/flowplayer/FlowPlayerDark.swf';
 	}else {
-		if (!isset($engine)) $engine='/lib/flowplayer/FlowPlayerClassic.swf';
+		if (!isset($engine)) $engine='lib/flowplayer/FlowPlayerClassic.swf';
 	}
 
-	if (!isset($configfile)) $configfile='/lib/flowplayer/flowPlayer.js';
+	if (!isset($configfile)) $configfile='lib/flowplayer/flowPlayer.js';
 
-
-	$flowplayer = "<embed WIDTH=\"$width\" HEIGHT=\"$height\" src=\"$engine\" TYPE=\"application/x-shockwave-flash\" ";
+	$flowplayer = '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" type="application/x-shockwave-flash" data="'.$engine.'" width="'.$width.'" height="'.$height.'">';
+	if (isset($videofile)) {
+		$flowplayer .= "<param name=\"flashvars\" value=\"config={autoPlay: $autoplay, videoFile:'$videofile',initialScale:'scale',useNativeFullScreen:true}\" />";
+	}
+	else {
+		$flowplayer .= "<param name=\"flashvars\" value=\"configFileName=$configfile\" />"; 
+	}
+	$flowplayer .= "<embed width=\"$width\" height=\"$height\" src=\"$engine\" type=\"application/x-shockwave-flash\" ";
 	$flowplayer .= "allowScriptAccess=\"sameDomain\" ";
 	$flowplayer .= "quality=\"high\" ";
 	$flowplayer .= "scale=\"noScale\" ";
 	if (isset($videofile)) {
-	$flowplayer .= "flashvars=\"config={autoPlay: false, videoFile:'$videofile',initialScale:'scale',useNativeFullScreen:true}\"\>"; 
+		$flowplayer .= "flashvars=\"config={autoPlay: $autoplay, videoFile:'$videofile',initialScale:'scale',useNativeFullScreen:true}\" />"; 
 	}
 	else {
-	$flowplayer .= "flashvars=\"configFileName=$configfile\">"; 
+		$flowplayer .= "flashvars=\"configFileName=$configfile\" />"; 
 	}
+	$flowplayer .= "</object>";
 
 	return $flowplayer;
 }
