28810b0938989b7ed62438c855d9cae9a0d18b74
luciash <luciash@b456876b-0849-0410-b77d-98878d47e9d5>
2009-03-27 01:50:08 +0000

    [FIX] some fixes and corrections to make it work not only in the root of a site but also when having tiki installed in a subdir + xhtml code fixes

--- mods/trunk/wiki-plugins/xspf_player/wiki-plugins/wikiplugin_xspf.php
+++ mods/trunk/wiki-plugins/xspf_player/wiki-plugins/wikiplugin_xspf.php
@@ -1,9 +1,9 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/xspf_player/wiki-plugins/wikiplugin_xspf.php,v 1.1 2006-11-29 12:07:33 ang23 Exp $
+// $Id: $
 
-// Wiki plugin to display a SWF file
-// damian aka damosoft 30 March 2004
+// \brief Wiki plugin to play mp3 files
+// @author damian aka damosoft, 30 March 2004
 
 function wikiplugin_xspf_help() {
         return tra("Displays an XSPF based Flash Music Player on the wiki page").":<br />~np~{XSPF(song=url_to_mp3,playlist=url_to_xspf_playlist,player=full|slim|button,autoplay=true|false,title=text)}{XSPF}~/np~";
@@ -23,25 +23,25 @@ function wikiplugin_xspf($data, $params) {
 	if ($player=='full') {
 	if (!isset($width)) $width=400;
 	if (!isset($height)) $height=168;
-	if (!isset($movie)) $movie='/lib/xspf_player/xspf_player.swf';
+	if (!isset($movie)) $movie='lib/xspf_player/xspf_player.swf';
 	} elseif ($player=='button') {
 	if (!isset($width)) $width=18;
 	if (!isset($height)) $height=18;
-	if (!isset($movie)) $movie='/lib/xspf_player/musicplayer.swf';
+	if (!isset($movie)) $movie='lib/xspf_player/musicplayer.swf';
 	} else {
 	if (!isset($width)) $width=400;
 	if (!isset($height)) $height=16;
-	if (!isset($movie)) $movie='/lib/xspf_player/xspf_player_slim.swf';
+	if (!isset($movie)) $movie='lib/xspf_player/xspf_player_slim.swf';
 	}
 
-	if (!isset($playlist)) $playlist='/lib/xspf_player/playlist.xspf';
+	if (!isset($playlist)) $playlist='lib/xspf_player/playlist.xspf';
 
 	if (isset($song)) {
-	    $xspf = "<OBJECT WIDTH=\"$width\" HEIGHT=\"$height\" DATA=\"$movie?song_url=$song&autoplay=$autoplay&autoload=$autoload&song_title='$title'\" TYPE=\"application/x-shockwave-flash\">";
-	$xspf .= "<PARAM VALUE=\"$movie?song_url=$song&autoplay=$autoplay&autoload=$autoload&song_title='$title'\" NAME=\"movie\"></PARAM></OBJECT>"; }
+	    $xspf = "<object width=\"$width\" height=\"$height\" data=\"$movie?song_url=$song&autoplay=$autoplay&autoload=$autoload&song_title='$title'\" type=\"application/x-shockwave-flash\">";
+	$xspf .= "<param value=\"$movie?song_url=$song&autoplay=$autoplay&autoload=$autoload&song_title='$title'\" name=\"movie\"></param></object>"; }
 	else {
-	$xspf = "<OBJECT WIDTH=\"$width\" HEIGHT=\"$height\" DATA=\"$movie?playlist_url=$playlist&autoplay=$autoplay&autoload=$autoload&player_title='$title'\" TYPE=\"application/x-shockwave-flash\">";
-	$xspf .= "<PARAM VALUE=\"$movie?playlist_url=$playlist&autoplay=$autoplay&autoload=$autoload&player_title='$title'\" NAME=\"movie\"></PARAM></OBJECT>";
+	$xspf = "<object width=\"$width\" height=\"$height\" data=\"$movie?playlist_url=$playlist&autoplay=$autoplay&autoload=$autoload&player_title='$title'\" type=\"application/x-shockwave-flash\">";
+	$xspf .= "<param value=\"$movie?playlist_url=$playlist&autoplay=$autoplay&autoload=$autoload&player_title='$title'\" name=\"movie\"></param></object>";
 	}
 
 	return $xspf;
