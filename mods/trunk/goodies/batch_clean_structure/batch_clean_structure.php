d04326a20105771eedcbeb6e369b9cb7a6837a4e
sylvieg <sylvieg@b456876b-0849-0410-b77d-98878d47e9d5>
2009-05-11 16:12:43 +0000

    [NEW]structure: batch to reassign the position of a node in a structure to have a coorect value displayed un the structure div in tiki-index.php

---
+++ mods/trunk/goodies/batch_clean_structure/batch_clean_structure.php
@@ -0,0 +1,50 @@
+<?php
+// $Id: /cvsroot/tikiwiki/tiki/tiki-view_sheets.php,v 1.16 2007-10-12 07:55:33 nyloth Exp $
+// This batch allows to fix the position recorded in the database to have a linear progression of these pos
+// (this was seen in tiki-index.php - the number was incorrect)
+  // Installation: put the batch in the tikiwiki root - after use, better to move or delete the script
+  // ***** Dump your database - just in case
+  // Run the script from the brower http://yoursite.com/batch_clean_structure.php?page_ref_id=x where x if the id of the structure
+  // or http://yoursite.com/batch_clean_structure.php to clean all the structures
+include_once('tiki-setup.php');
+if($prefs['feature_wiki'] != 'y') {
+    $smarty->assign('msg', tra('This feature is disabled').': feature_wiki');
+    $smarty->display('error.tpl');
+    die;  
+}
+if($prefs['feature_wiki_structure'] != 'y') {
+    $smarty->assign('msg', tra('This feature is disabled').': feature_wiki_structure');
+    $smarty->display('error.tpl');
+    die;  
+}
+include_once ('lib/structures/structlib.php');
+
+if (isset($_REQUEST['page_ref_id'])) {
+	if (is_array($_REQUEST['page_ref_id'])) {
+		$list_structures['data'] = $_REQUEST['page_ref_id'];
+	} else {
+		$list_structures['data'] = array($_REQUEST['page_ref_id']);
+	}
+} else {
+	$list_structures = $structlib->list_structures(0, -1, 'pageName_asc');
+}
+//print_r($list_structures);
+
+$query = "update `tiki_structures` set `pos`=? where `page_ref_id`=?";
+foreach ($list_structures['data'] as $page_ref_id) {
+	if (is_array($page_ref_id) && isset($page_ref_id['page_ref_id'])) {
+		$page_ref_id = $page_ref_id['page_ref_id'];
+	}
+	$tree = $structlib->get_subtree($page_ref_id);
+	foreach ($tree as $node) {
+		//echo '<pre>'; print_r($node); echo '</pre>';
+		if (!empty($node['pos']) && ! empty($node['db_pos'])) {
+			$nodes = split('\.', $node['pos']);
+			$last = $nodes[count($nodes) - 1];
+			if ($last != $node['db_pos']) {
+				echo $node['pageName'].':'.$node['db_pos'].'->'.$last.'<br>';
+				$tikilib->query($query, array($last, $node['page_ref_id']));
+			}
+		}
+	}
+}
