afd47d8981fd1ea40047858a17701111b6b22476
sylvieg <sylvieg@b456876b-0849-0410-b77d-98878d47e9d5>
2009-05-12 11:08:31 +0000

    some more messages

--- mods/trunk/goodies/batch_clean_structure/batch_clean_structure.php
+++ mods/trunk/goodies/batch_clean_structure/batch_clean_structure.php
@@ -7,7 +7,6 @@
   // Run the script from the brower http://yoursite.com/batch_clean_structure.php?page_ref_id=x where x if the id of the structure
   // or http://yoursite.com/batch_clean_structure.php to clean all the structures
 include_once('tiki-setup.php');
-include_once ('lib/structures/structlib.php');
 if($prefs['feature_wiki'] != 'y') {
     $smarty->assign('msg', tra('This feature is disabled').': feature_wiki');
     $smarty->display('error.tpl');
@@ -32,20 +31,24 @@ if (isset($_REQUEST['page_ref_id'])) {
 //print_r($list_structures);
 
 $query = "update `tiki_structures` set `pos`=? where `page_ref_id`=?";
+$modif = 0;
 foreach ($list_structures['data'] as $page_ref_id) {
 	if (is_array($page_ref_id) && isset($page_ref_id['page_ref_id'])) {
 		$page_ref_id = $page_ref_id['page_ref_id'];
 	}
 	$tree = $structlib->get_subtree($page_ref_id);
 	foreach ($tree as $node) {
-		//echo '<pre>'; print_r($node); echo '</pre>';
+		echo $node['pos']. ' '.$node['pageName'].'<br />';
 		if (!empty($node['pos']) && ! empty($node['db_pos'])) {
 			$nodes = split('\.', $node['pos']);
 			$last = $nodes[count($nodes) - 1];
 			if ($last != $node['db_pos']) {
-				echo $node['pageName'].':'.$node['db_pos'].'->'.$last.'<br>';
+				++$modif;
+				echo '------------->'.$node['db_pos'].'->'.$last.'<br />';
 				$tikilib->query($query, array($last, $node['page_ref_id']));
 			}
 		}
 	}
+	echo '<br />';
 }
+echo "$modif modifications";
