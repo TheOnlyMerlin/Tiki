TheOnlyMerlin/Tiki

commit 700b5bb62c2a698e94a4d5dbcecd8c5daaf1e7d5

[FIX] Was counting result of comparison, rather than comparing the count. Also stopped it querying with no WHERE statement causing an error (once the above was fixed)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@20160 b456876b-0849-0410-b77d-98878d47e9d5

lib/tikilib.php
@@ -4729,8 +4729,9 @@ class TikiLib extends TikiDb {
 		}
 
 		$cond_query = '';
+		$result = null;
 		if ( is_null($bindvars) ) $bindvars = array();
-		if ( count($needed > 0 ) ) {
+		if ( count($needed) > 0 ) {
 			foreach ( $needed as $var => $def ) {
 				if ( $cond_query != '' ) {
 					$cond_query .= ' or ';
@@ -4740,9 +4741,10 @@ class TikiLib extends TikiDb {
 				$cond_query .= "`$field_name`=?";
 				$bindvars[] = $var;
 			}
+
+			$query = "select `$field_name`, `value` from `$table` where $query_cond $cond_query";
+			$result = $this->query($query, $bindvars);
 		}
-		$query = "select `$field_name`, `value` from `$table` where $query_cond $cond_query";
-		$result = $this->query($query, $bindvars);
 
 		if ( $result ) {
 			while ( $res = $result->fetchRow() ) {

commit 1b906e082d3bdb9e29b6c4da966e781bb4919045

[FIX] Query needed to be outside the $needed loop but added dummy where condition to prevent SQL error Fixes problem introduced in r20160 (by me)

lib/tikilib.php
@@ -4731,6 +4731,9 @@ class TikiLib extends TikiDb_Bridge {
 		}
 
 		$cond_query = '';
+		if (empty($query_cond)) {
+			$query_cond = '1';
+		}
 		$result = null;
 		if ( is_null($bindvars) ) $bindvars = array();
 		if ( count($needed) > 0 ) {
@@ -4743,11 +4746,10 @@ class TikiLib extends TikiDb_Bridge {
 				$cond_query .= "`$field_name`=?";
 				$bindvars[] = $var;
 			}
-
-			$query = "select `$field_name`, `value` from `$table` where $query_cond $cond_query";
-			$result = $this->query($query, $bindvars);
 		}
-
+		$query = "select `$field_name`, `value` from `$table` where $query_cond $cond_query";
+		$result = $this->query($query, $bindvars);
+		
 		if ( $result ) {
 			while ( $res = $result->fetchRow() ) {
 				// store the db value in the global array