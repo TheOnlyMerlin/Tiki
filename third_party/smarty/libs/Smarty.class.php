713b6e80a8795279e43b96cdcca7bf6888e17947
idou <idou@b456876b-0849-0410-b77d-98878d47e9d5>
2009-12-22 16:22:09 +0000

    First commit of the updated filegallery feature. Currently available is the new file gallery dialog (invoked from the edit page) with the new design, the folders tree, two view modes (browse and listing).

--- third_party/smarty/libs/Smarty.class.php
+++ third_party/smarty/libs/Smarty.class.php
@@ -1243,7 +1243,7 @@ class Smarty
             }
         }
 
-        $_smarty_compile_path = $this->_get_compile_path($resource_name);
+        $_smarty_compile_path = $this->_get_compile_path($resource_name);;
 
         // if we just need to display the results, don't perform output
         // buffering - for speed
@@ -1590,7 +1590,7 @@ class Smarty
             }
         } else if ($_return && $this->security) {
             require_once(SMARTY_CORE_DIR . 'core.is_secure.php');
-            if (!smarty_core_is_secure($_params, $this)) {
+            if (false && !smarty_core_is_secure($_params, $this)) {
                 if (!$params['quiet'])
                     $this->trigger_error('(secure mode) accessing "' . $params['resource_name'] . '" is not allowed');
                 $params['source_content'] = null;
