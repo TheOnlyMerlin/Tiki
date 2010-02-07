4426e8e177677db4b910657f37a48dd314ee1885
sylvieg <sylvieg@b456876b-0849-0410-b77d-98878d47e9d5>
2010-02-07 15:10:09 +0000

    [MOD]submit: urlencode the parameters + allow a field title (totdo update all the networks urls) + param showonly to limit the networoks

--- mods/trunk/wiki-plugins/submit/wiki-plugins/wikiplugin_submit.php
+++ mods/trunk/wiki-plugins/submit/wiki-plugins/wikiplugin_submit.php
@@ -22,13 +22,30 @@ function wikiplugin_submit_info() {
 			'image' => array(
 				'required' => 'n',
 				'name' => tra('Show image'),
-				'description' => 'y '.tra('or').' n'),
+				'description' => 'y '.tra('or').' n',
+				'filter' => 'alpha'
+			),
 			'text' => array(
 				'required' => 'n',
 				'name' => tra('Show text'),
-				'description' => 'y '.tra('or').' n'),
+				'description' => 'y '.tra('or').' n',
+				'filter' => 'alpha'
 			),
-		 );
+			'showonly' => array(
+				'required' => 'n',
+				'name' => tra('Show only some networks'),
+				'description' => tra('Names separated with comma'),
+				'filter' => 'alpha',
+				'separator' => ','
+			),
+			'title' => array(
+				'required' => 'n',
+				'name' => tra('Title to be shown for networkink allowing it'),
+				'description' => tra('Title to be shown for networkink allowing it'),
+				'filter' => 'text',
+			),
+		)
+	);
 }
 
 function wikiplugin_submit($data, $params) {
@@ -48,111 +65,161 @@ function wikiplugin_submit($data, $params) {
 		$text = 'n';
 	}
 
-//names of the sites. used as ALT and TITLE text. shown if text=y
-//you can add or remove items, as desired
-$title = array(
-	'Digg',
-	'del.icio.us',
-	'Blinklist',
-	'Furl',
-	'Reddit',
-	'Blogmarks',
-	'Magnolia',
-	'Sphere',
-	'Yahoo!',
-	'Technorati',
-	'Blue Dot',
-	'Simpy',
-	'Newsvine',
-	'Stumble Upon',
-	'co.mments.com',
-	'Blinkbits',
-	'BlogMemes',
-	'Connotea',
-    'Mister Wong',
-	'Facebook',
-	'Google'
-);
-
-//icons of the sites. assumed to be 16x16 pixels. shown if image=y
-//for each TITLE, there must be a matching ICON
-//icons must be located in <TIKI_HOME>/img/icons2/ directory
-$icon = array(
-	'digg.png',
-	'delicious.png',
-	'blink.png',
-	'furl.png',
-	'reddit.png',
-	'blogmarks.png',
-	'magnolia.png',
-	'sphere.png',
-	'yahoo.png',
-	'technorati.png',
-	'bluedot.png',
-	'simpy.png',
-	'newsvine.png',
-	'stumbleupon.png',
-	'comments.png',
-	'blinkbits.png',
-	'blogmemes.png',
-	'connotea.png',
-    'mrwong.png',
-	'facebook.png',
-	'google.png'
-);
-
-//submission url of the site. refer to each site's faqs for exact url.
-//for each TITLE, there must be a matching URL
-$url = array(
-	'http://digg.com/submit?phase=2&url=http://',
-	'https://secure.del.icio.us/login?url=http://',
-	'http://www.blinklist.com/index.php?Action=Blink/addblink.php&Url=http://',
-	'http://www.furl.net/storeIt.jsp?u=http://',
-	'http://reddit.com/submit?url=http://',
-	'http://blogmarks.net/my/new.php?mini=1&simple=1&url=http://',
-	'http://ma.gnolia.com/bookmarklet/add?url=http://',
-	'http://www.sphere.com/search?q=sphereit:http://',
-	'http://myweb2.search.yahoo.com/myresults/bookmarklet?u=http://',
-	'http://technorati.com/faves/?add=http://',
-	'http://bluedot.us/Authoring.aspx?u=http://',
-	'http://www.simpy.com/simpy/LinkAdd.do?href=http://',
-	'http://www.newsvine.com/_tools/seed&save?u=http://',
-	'http://www.stumbleupon.com/submit?url=http://',
-	'http://co.mments.com/track?url=http://',
-	'http://www.blinkbits.com/bookmarklets/save.php?v=1&source_url=http://',
-	'http://www.blogmemes.net/post.php?url=http://',
-	'http://www.connotea.org/addpopup?continue=confirm&uri=http://',
-    'http://www.mister-wong.com/addurl/?bm_url=http://',
-	'http://www.facebook.com/share.php?u=http://',
-	'http://www.google.com/bookmarks/mark?op=edit&amp;output=popup&amp;bkmk=http://'
-);
-
+// title : naem of then site will appear in title and alt
+// icons of the sites. assumed to be 16x16 pixels. shown if image=y
+// icons must be located in <TIKI_HOME>/img/icons2/ directory
+// link: submission url of the site. refer to each site's faqs for exact url.
+	$networks = array(
+					array(
+						'title'=>'Digg',
+						'icon'=>'digg.png',
+						'url'=>'http://digg.com/submit?phase=2&url=%s',
+						  ),
+					array(
+						'title'=>'del.icio.us',
+						'icon'=>'delicious.png',
+						'url'=>'https://secure.del.icio.us/login?url=%s',
+						  ),
+					array(
+						'title'=>'Blinklist',
+						'icon'=>'blink.png',
+						'url'=>'http://www.blinklist.com/index.php?Action=Blink/addblink.php&amp;Url=%s',
+						  ),
+					array(
+						'title'=>'Furl',
+						'icon'=>'furl.png',
+						'url'=>'http://www.furl.net/storeIt.jsp?u=%s',
+						  ),
+					array(
+						'title'=>'Reddit',
+						'icon'=>'reddit.png',
+						'url'=>'http://reddit.com/submit?url=%s',
+						  ),
+					array(
+						'title'=>'Blogmarks',
+						'icon'=>'blogmarks.png',
+						'url'=>'http://blogmarks.net/my/new.php?mini=1&amp;simple=1&amp;url=%s',
+						  ),
+					array(
+						'title'=>'Magnolia',
+						'icon'=>'magnolia.png',
+						'url'=>'http://ma.gnolia.com/bookmarklet/add?url=%s',
+						  ),
+					array(
+						'title'=>'Sphere',
+						'icon'=>'sphere.png',
+						'url'=>'http://www.sphere.com/search?q=sphereit:%s',
+						  ),
+					array(
+						'title'=>'Yahoo!',
+						'icon'=>'yahoo.png',
+						'url'=>'http://myweb2.search.yahoo.com/myresults/bookmarklet?u=%s',
+						  ),
+					array(
+						'title'=>'Technorati',
+						'icon'=>'technorati.png',
+						'url'=>'http://technorati.com/faves/?add=%s',
+						  ),
+					array(
+						'title'=>'Blue Dot',
+						'icon'=>'bluedot.png',
+						'url'=>'http://bluedot.us/Authoring.aspx?u=%s',
+						  ),
+					array(
+						'title'=>'Simpy',
+						'icon'=>'simpy.png',
+						'url'=>'http://www.simpy.com/simpy/LinkAdd.do?href=%s',
+						  ),
+					array(
+						'title'=>'Newsvine',
+						'icon'=>'newsvine.png',
+						'url'=>'http://www.newsvine.com/_tools/seed&save?u=%s',
+						  ),
+					array(
+						'title'=>'Stumble Upon',
+						'icon'=>'stumbleupon.png',
+						'url'=>'http://www.stumbleupon.com/submit?url=%s',
+						  ),
+					array(
+						'title'=>'co.mments.com',
+						'icon'=>'comments.png',
+						'url'=>'http://co.mments.com/track?url=%s',
+						  ),
+					array(
+						'title'=>'Blinkbits',
+						'icon'=>'blinkbits.png',
+						'url'=>'http://www.blinkbits.com/bookmarklets/save.php?v=1&amp;source_url=%s',
+						  ),
+					array(
+						'title'=>'BlogMemes',
+						'icon'=>'blogmemes.png',
+						'url'=>'http://www.blogmemes.net/post.php?url=%s',
+						  ),
+					array(
+						'title'=>'Connotea',
+						'icon'=>'connotea.png',
+						'url'=>'http://www.connotea.org/addpopup?continue=confirm&uri=%s',
+						  ),
+					array(
+						'title'=>'Mister Wong',
+						'icon'=>'mrwong.png',
+						'url'=>'http://www.mister-wong.com/addurl/?bm_url=%s',
+						  ),
+					array(
+						'title'=>'Facebook',
+						'icon'=>'facebook.png',
+						'url'=>'http://www.facebook.com/share.php?u=%s',
+						'url_title'=>'http://www.facebook.com/share.php?t=%s&amp;u=%s',
+						  ),
+					array(
+						'title'=>'Google',
+						'icon'=>'google.png',
+						'url'=>'http://www.google.com/bookmarks/mark?op=edit&amp;output=popup&amp;bkmk=%s',
+						'url_title'=>'http://www.google.com/bookmarks/mark?op=edit&amp;output=popup&amp;title=%s&amp;bkmk=%s',
+						  ),
+					array(
+						'title'=>'Twitter',
+						'icon'=>'twitter.png',
+						'url'=>'http://twitter.com/home?status=%s',/* do not put www.twitter */
+						'url_title'=>'http://twitter.com/home?status=%s%s',
+						  ),
+		);
 //get the current tiki page and server
-$my = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+$my = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+$my = 'http://droit-inc.com';
 
 //build the submission links
 $result = "<p>".tra('Share this page:')."</p><p class='mini'>";
 
 //loop for each $title
-for ($i = 0; $i < count($title); $i++) {
-	$result .= "&nbsp;<a href='".$url[$i].$my."' title='".tra('Submit to '). $title[$i] . "'>";
+$first = true;
+foreach ($networks as $network) {
+	if ($first) {
+		$first = false;
+	} elseif ($text == 'y') {
+		$result .= '&nbsp;|&nbsp;';
+	}
+	if (!empty($showonly) && !in_array($network['title'], $showonly)) {
+		continue;
+	}
+	if (!empty($network['url_title']) && !empty($title)) {
+		$href = sprintf($network['url_title'], urlencode($title), urlencode($my));
+	} else {
+		$href = sprintf($network['url'], urlencode($my));
+	}
+	$result .= "&nbsp;<a href='".$href."' title='".sprintf(tra('Submit to %s'), $network['title']) . "'>";
 
 //show the image?
 	if ($image == 'y'){
-		$result .= "<img src='img/icons2/" . $icon[$i] . "' width='16' height='16' border='0' alt='" . $title[$i] . "' />";
+		$result .= "<img src='img/icons2/" . $network['icon'] . "' width='16' height='16' border='0' alt='" . $network['title'] . "' />";
 	}
 
 //show the text?
 	if ($text == 'y'){
-		$result .= "&nbsp;" . $title[$i];
+		$result .= "&nbsp;" . $network['title'];
 	}
 
-$result .= "</a>&nbsp;";
-
-	if (($text == 'y') and (next($title))){
-		$result .= "&nbsp;|&nbsp;";
-	}
-
+	$result .= "</a>&nbsp;";
 }
 
 $result .= "</p>";
@@ -160,5 +227,3 @@ $result .= "</p>";
 return $result;
 
 }
-
-?>
