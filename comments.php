TheOnlyMerlin/Tiki

commit 38e313ca08fb5528e04ca09f25f4c6ba1b2926cc

[MOD]watch: cleaning - replace hash with an autoincrement id
git-svn-id: file:///var/local/code.tiki.org/tikisvn/branches/1.10/tiki@11994 b456876b-0849-0410-b77d-98878d47e9d5

lib/tikilib.php
@@ -1,5 +1,5 @@
 <?php
-// CVS: $Id: tikilib.php,v 1.801.2.87 2008-03-11 22:01:13 lphuberdeau Exp $
+// CVS: $Id: tikilib.php,v 1.801.2.88 2008-03-13 21:00:48 sylvieg Exp $
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
@@ -195,23 +195,22 @@ class TikiLib extends TikiDB {
     /*shared*/
     function add_user_watch($user, $event, $object, $type, $title, $url, $email='') {
 
-	$hash = md5(uniqid('.'));
 	if (empty($email)) {
 		global $userlib;
 		$email = $userlib->get_user_email($user);
 	}
 	$query = "delete from `tiki_user_watches` where ".$this->convert_binary()." `user`=? and `event`=? and `object`=?";
 	$this->query($query,array($user,$event,$object));
-	$query = "insert into `tiki_user_watches`(`user`,`event`,`object`,`email`,`hash`,`type`,`title`,`url`) ";
-	$query.= "values(?,?,?,?,?,?,?,?)";
-	$this->query($query,array($user,$event,$object,$email,$hash,$type,$title,$url));
+	$query = "insert into `tiki_user_watches`(`user`,`event`,`object`,`email`,`type`,`title`,`url`) ";
+	$query.= "values(?,?,?,?,?,?,?)";
+	$this->query($query,array($user,$event,$object,$email,$type,$title,$url));
 	return true;
     }
 
     /*shared*/
-    function remove_user_watch_by_hash($hash) {
-	$query = "delete from `tiki_user_watches` where `hash`=?";
-	$this->query($query,array($hash));
+    function remove_user_watch_by_id($id) {
+	$query = "delete from `tiki_user_watches` where `watchId`=?";
+	$this->query($query,array($id));
     }
 
     /*shared*/
@@ -4312,6 +4311,12 @@ function add_pageview() {
 		global $prefs;
 		return isset($prefs[$name]) ? $prefs[$name] : $default;
     }
+	function delete_preference($name) {
+		global $prefs;
+		$query = "delete from `tiki_preferences` where `name`=?";
+		$this->query($query,array($name));
+		$this->set_lastUpdatePrefs();
+	}
 
     function set_preference($name, $value) {
 		global $user_overrider_prefs, $user_preferences, $user, $prefs;
@@ -4346,7 +4351,6 @@ function add_pageview() {
 	function set_lastUpdatePrefs() {
 		$query = "update `tiki_preferences` set `value`=`value`+1 where `name`=?";
 		$this->query($query, array('lastUpdatePrefs'));
-		
 	}
 
 	function _get_values($table, $field_name, $var_names = null, &$global_ref, $query_cond = '', $bindvars = null) {

templates/tiki-user_watches.tpl
@@ -1,4 +1,4 @@
-{* $Header: /cvsroot/tikiwiki/tiki/templates/tiki-user_watches.tpl,v 1.24.2.2 2008-01-30 15:33:51 nyloth Exp $ *}
+{* $Header: /cvsroot/tikiwiki/tiki/templates/tiki-user_watches.tpl,v 1.24.2.3 2008-03-13 21:00:48 sylvieg Exp $ *}
 <h1><a class="pagetitle" href="tiki-user_watches.php">{tr}User Watches{/tr}</a>
 
 {if $prefs.feature_help eq 'y'}
@@ -66,7 +66,7 @@
 {section name=ix loop=$watches}
 <tr>
 <td style="text-align:center;" class="{cycle advance=false}">
-<input type="checkbox" name="watch[{$watches[ix].hash}]" />
+<input type="checkbox" name="watch[{$watches[ix].watchId}]" />
 </td>
 <td class="{cycle advance=false}">
 	{if $watches[ix].event eq 'article_submitted'}

tiki-user_watches.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-user_watches.php,v 1.21 2007-10-12 07:55:32 nyloth Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-user_watches.php,v 1.21.2.1 2008-03-13 21:00:48 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -26,11 +26,11 @@ if ($prefs['feature_user_watches'] != 'y') {
 }
 
 
-if (isset($_REQUEST['hash'])) {
+if (isset($_REQUEST['id'])) {
   $area = 'deluserwatch';
   if ($prefs['feature_ticketlib2'] != 'y' or (isset($_POST['daconfirm']) and isset($_SESSION["ticket_$area"]))) {
     key_check($area);
-		$tikilib->remove_user_watch_by_hash($_REQUEST['hash']);
+		$tikilib->remove_user_watch_by_id($_REQUEST['id']);
   } else {
     key_get($area);
   }
@@ -60,7 +60,7 @@ if (isset($_REQUEST["delete"]) && isset($_REQUEST['watch'])) {
   if ($prefs['feature_ticketlib2'] != 'y' or (isset($_POST['daconfirm']) and isset($_SESSION["ticket_$area"]))) {
     key_check($area); */
 	foreach (array_keys($_REQUEST["watch"])as $item) {
-		$tikilib->remove_user_watch_by_hash($item);
+		$tikilib->remove_user_watch_by_id($item);
 	}
 /*  } else {
 	key_get($area);

db/tiki.sql
@@ -1,6 +1,6 @@
 # $Rev$
-# $Date: 2008-03-11 22:01:11 $
-# $Author: lphuberdeau $
+# $Date: 2008-03-13 21:00:46 $
+# $Author: sylvieg $
 # $Name: not supported by cvs2svn $
 # phpMyAdmin MySQL-Dump
 # version 2.5.1
@@ -3911,14 +3911,15 @@ CREATE TABLE tiki_user_votings (
 
 DROP TABLE IF EXISTS tiki_user_watches;
 CREATE TABLE tiki_user_watches (
+  watchId int(12) NOT NULL auto_increment,
   user varchar(200) NOT NULL default '',
   event varchar(40) NOT NULL default '',
   object varchar(200) NOT NULL default '',
-  hash varchar(32) default NULL,
   title varchar(250) default NULL,
   type varchar(200) default NULL,
   url varchar(250) default NULL,
   email varchar(200) default NULL,
+  KEY (watchId),
   PRIMARY KEY  (`user`(100),event,object(100))
 ) ENGINE=MyISAM;
 # --------------------------------------------------------

db/tiki_1.9to1.10.sql
@@ -1,4 +1,4 @@
-# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.221.2.43 2008-03-11 22:01:12 lphuberdeau Exp $
+# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.221.2.44 2008-03-13 21:00:47 sylvieg Exp $
 
 # The following script will update a tiki database from version 1.9 to 1.10
 # 
@@ -1675,3 +1675,7 @@ CREATE TABLE tiki_pages_changes (
 
 #2008-03-11 nyloth
 DELETE FROM tiki_preferences WHERE name='fgal_list_parent';
+
+#2008-03-13 sylvieg
+ALTER TABLE tiki_user_watches ADD watchId int(12) NOT NULL auto_increment FIRST, ADD  KEY (`watchId`);
+alter table tiki_user_watches drop hash;

templates/tiki-admin_notifications.tpl
@@ -77,8 +77,8 @@
 <td>{$channels[user].email}</td>
 <td>{$channels[user].user}</td>
 <td>
-   <a class="link" href="tiki-admin_notifications.php?offset={$offset}&amp;sort_mode={$sort_mode}&amp;removeevent={$channels[user].hash}">{icon _id='cross' alt='{tr}Remove{/tr}'}</a>
-   <input type="checkbox" name="checked[]" value="{$channels[user].hash|escape}" {if $smarty.request.checked and in_array($channels[user].hash,$smarty.request.checked)}checked="checked"{/if} />
+   <a class="link" href="tiki-admin_notifications.php?offset={$offset}&amp;sort_mode={$sort_mode}&amp;removeevent={$channels[user].watchId}">{icon _id='cross' alt='{tr}Remove{/tr}'}</a>
+   <input type="checkbox" name="checked[]" value="{$channels[user].watchId|escape}" {if $smarty.request.checked and in_array($channels[user].watchId,$smarty.request.checked)}checked="checked"{/if} />
 </td>
 </tr>
 {sectionelse}

tiki-admin_notifications.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_notifications.php,v 1.21.2.1 2007-11-05 16:20:30 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_notifications.php,v 1.21.2.2 2008-03-13 21:00:48 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -93,7 +93,7 @@ if (isset($_REQUEST["removeevent"])) {
   $area = 'delnotif';
   if ($prefs['feature_ticketlib2'] != 'y' or (isset($_POST['daconfirm']) and isset($_SESSION["ticket_$area"]))) {
     key_check($area);
-		$tikilib->remove_user_watch_by_hash($_REQUEST["removeevent"]);
+		$tikilib->remove_user_watch_by_id($_REQUEST["removeevent"]);
   } else {
     key_get($area);
   }
@@ -101,7 +101,7 @@ if (isset($_REQUEST["removeevent"])) {
 if (isset($_REQUEST['delsel_x']) && isset($_REQUEST['checked'])) {
 	check_ticket('admin-notif');
 	foreach($_REQUEST['checked'] as $id) {
-		$tikilib->remove_user_watch_by_hash($id);
+		$tikilib->remove_user_watch_by_id($id);
 	}
  }	
 

commit 4332bf20138f4b4d2f90a011f1452f09d85694de

[QT] Quality Team Backport
r24721 | pkdille | 2010-01-28 21:31:23 +0100 (Thu, 28 Jan 2010) | 6 lines
[bp/r24720][FIX] mail notifications : fix the link sent in the notification email which gives the possibility to unsubscribe the user watch.
     BTW, fix the remove_user_watch_by_id function in order to not allow anybody to remove every notification.
     This was broken since r11994 and was ticket 2895 in dev tracker.
     Thks to nyloth and sept_7 for the help.

comments.php
@@ -223,7 +223,7 @@ if ( isset($_REQUEST['comments_objectId']) && $_REQUEST['comments_objectId'] == 
 						$smarty->assign('mail_user', $user);
 						$smarty->assign('mail_title', $_REQUEST["comments_title"]);
 						$smarty->assign('mail_comment', $_REQUEST["comments_data"]);
-						$smarty->assign('mail_hash', $not['hash']);
+						$smarty->assign('watchId', $not['watchId']);
 						$foo = parse_url($_SERVER["REQUEST_URI"]);
 						$machine = $tikilib->httpPrefix(). dirname( $foo["path"] );
 						$smarty->assign('mail_machine', $machine);

lib/notifications/notificationemaillib.php
@@ -275,8 +275,7 @@ function sendWikiEmailNotification($wikiEvent, $pageName, $edit_user, $edit_comm
 	    }
 
 	    foreach ($nots as $not) {
-			if (isset($not['hash']))
-				$smarty->assign('mail_hash', $not['hash']);
+				$smarty->assign('watchId', $not['watchId']);
 
 			$mail_subject = $smarty->fetchLang($not['language'], "mail/user_watch_wiki_page_changed_subject.tpl");
 			$mail_data = $smarty->fetchLang($not['language'], "mail/user_watch_wiki_page_changed.tpl");

lib/tikilib.php
@@ -249,10 +249,30 @@ class TikiLib extends TikiDb_Bridge {
 		return true;
 	}
 
+	/**
+	 * get_user_notification: returns the owner (user) related to a watchId 
+	 * 
+	 * @param mixed $id watchId 
+	 * @access public
+	 * @return the user login related to the watchId
+	 */
+	function get_user_notification($id) {
+
+		$query = "select `user` from `tiki_user_watches` where `watchId`=?";
+		return $this->getOne($query, array($id));
+
+	}
 	/*shared*/
 	function remove_user_watch_by_id($id) {
-		$query = "delete from `tiki_user_watches` where `watchId`=?";
-		$this->query($query,array($id));
+		global $tiki_p_admin_notifications, $user;
+		if ( $tiki_p_admin_notifications === 'y' or $user === $this->get_user_notification($id) ) {
+			$query = "delete from `tiki_user_watches` where `watchId`=?";
+			$this->query($query, array($id));
+
+			return true;
+		}
+
+		return false;
 	}
 
 	function remove_group_watch_by_id($id) {

lib/wiki/wikilib.php
@@ -308,6 +308,7 @@ class WikiLib extends TikiLib {
 				$smarty->assign('mail_newname', $newName);
 				$smarty->assign('mail_date', $this->now);
 				$smarty->assign('mail_user', $user);
+				$smarty->assign('watchId', $nots['watchId']);
 				$foo = parse_url($_SERVER["REQUEST_URI"]);
 				$machine = $tikilib->httpPrefix(). $foo["path"];
 				$smarty->assign('mail_machine', $machine);

templates/mail/user_watch_article_post.tpl
@@ -7,10 +7,8 @@
 {if $mail_action neq 'Delete'}{tr}View the article at:{/tr}
 {$mail_machine_raw}/{$mail_postid|sefurl:article}
 {/if}
-{if $mail_user ne 'admin'}
 
 {tr}If you don't want to receive these notifications follow this link:{/tr}
-{$mail_machine_raw}/tiki-user_watches.php?hash={$mail_hash}
-{/if}
+{$mail_machine_raw}/tiki-user_watches.php?id={$watchId}
 {$mail_data}
 

templates/mail/user_watch_blog_post.tpl
@@ -7,5 +7,4 @@
 {$mail_machine_raw}/tiki-view_blog_post.php?blogId={$mail_blogid}&postId={$mail_postid}
 
 {tr}If you don't want to receive these notifications follow this link:{/tr}
-{$mail_machine_raw}/tiki-user_watches.php?hash={$mail_hash}
-
+{$mail_machine_raw}/tiki-user_watches.php?id={$watchId}

templates/mail/user_watch_map_changed.tpl
@@ -7,4 +7,4 @@
 {$mail_machine}?mapfile={$mail_page}
 
 {tr}If you don't want to receive these notifications follow this link:{/tr}
-{$mail_machine_raw}/tiki-user_watches.php?hash={$mail_hash}
+{$mail_machine_raw}/tiki-user_watches.php?id={$watchId}

templates/mail/user_watch_wiki_new_attach.tpl
@@ -9,4 +9,4 @@
 {$mail_machine_raw}/tiki-index.php?page={$mail_page|escape:'url'}
 
 {tr}If you don't want to receive these notifications follow this link:{/tr}
-{$mail_machine_raw}/tiki-user_watches.php?hash={$mail_hash}
+{$mail_machine_raw}/tiki-user_watches.php?id={$watchId}

templates/mail/user_watch_wiki_page_changed.tpl
@@ -18,8 +18,9 @@
 {elseif $mail_action eq 'attach'}{$mail_data} : {$mail_machine_raw}/tiki-download_wiki_attachment.php?attId={$mail_attId}
 {/if}
 
-{if $mail_hash}{tr}If you don't want to receive these notifications follow this link:{/tr}
-{$mail_machine_raw}/tiki-user_watches.php?hash={$mail_hash}
+{if $watchId}
+	{tr}If you don't want to receive these notifications follow this link:{/tr}
+	{$mail_machine_raw}/tiki-user_watches.php?id={$watchId}
 {/if}
 
 ***********************************************************

templates/mail/user_watch_wiki_page_comment.tpl
@@ -4,7 +4,8 @@
 {$mail_machine_raw}/tiki-index.php?page={$mail_page|escape:"url"}#comments
 {tr}Title:{/tr} {$mail_title}
 {tr}Comment:{/tr} {$mail_comment}
-{if $mail_hash}
+
+{if $watchId}
 {tr}If you don't want to receive these notifications follow this link:{/tr}
-{$mail_machine_raw}/tiki-user_watches.php?hash={$mail_hash}
+{$mail_machine_raw}/tiki-user_watches.php?id={$watchId}
 {/if}

templates/mail/user_watch_wiki_page_renamed.tpl
@@ -6,4 +6,4 @@
 {tr}New name{/tr}: {$mail_newname}
 
 {tr}If you don't want to receive these notifications follow this link:{/tr}
-{$mail_machine_raw}/tiki-user_watches.php?hash={$mail_hash}
+{$mail_machine_raw}/tiki-user_watches.php?id={$watchId}

templates/tiki-user_watches.tpl
@@ -12,7 +12,11 @@
 <h2>{tr}Report Preferences{/tr}</h2>
 {if $prefs.feature_daily_report_watches eq 'y'}
 
-{remarksbox type="tip" title="{tr}Tip{/tr}"}{tr}Use reports to summarise notifications about objects you are watching.{/tr}{/remarksbox}
+{if $remove_user_watch_error}
+	{remarksbox type="error" title="{tr}Error{/tr}"}{tr}You are not allowed to remove this notification !{/tr}{/remarksbox}
+{else}
+	{remarksbox type="tip" title="{tr}Tip{/tr}"}{tr}Use reports to summarise notifications about objects you are watching.{/tr}{/remarksbox}
+{/if}
 
 <form action="tiki-user_reports.php" method="post" id='formi'>
 	<input type="hidden" name="report_preferences" value="true"/>

tiki-map_edit.php
@@ -159,7 +159,7 @@ if (isset($_REQUEST["save"])) {
 			$smarty->assign('mail_page', $_REQUEST["mapfile"]);
 			$smarty->assign('mail_date', date("U"));
 			$smarty->assign('mail_user', $user);
-			$smarty->assign('mail_hash', $not['hash']);
+			$smarty->assign('watchId', $not['watchId']);
 			$foo = parse_url($_SERVER["REQUEST_URI"]);
 			$machine = $tikilib->httpPrefix() . $foo["path"];
 			$smarty->assign('mail_machine', $machine);

tiki-user_watches.php
@@ -47,7 +47,8 @@ if (isset($_REQUEST['id'])) {
 	$area = 'deluserwatch';
 	if ($prefs['feature_ticketlib2'] != 'y' or (isset($_POST['daconfirm']) and isset($_SESSION["ticket_$area"]))) {
 		key_check($area);
-		$tikilib->remove_user_watch_by_id($_REQUEST['id']);
+		$error = $tikilib->remove_user_watch_by_id($_REQUEST['id']);
+		$smarty->assign('remove_user_watch_error', !$error);
 	} else {
 		key_get($area);
 	}