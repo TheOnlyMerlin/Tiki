d44b95947b562634b2132a3884fdb0ef3b0502f7
sept_7 <sept_7@b456876b-0849-0410-b77d-98878d47e9d5>
2010-02-24 14:49:36 +0000

    [FIX] Copyright notice, No PHP end tag

--- third_party/jscalendar/index.php
+++ third_party/jscalendar/index.php
@@ -1,14 +1,9 @@
 <?php
-
-// $Id: /cvsroot/tikiwiki/_jscalendar/index.php,v 1.4.2.1 2007-11-04 22:08:32 nyloth Exp $
-
-// Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-
-// This redirects to the sites root to prevent directory browsing
+// $Id: $
 
 header ("location: ../index.php");
 die;
-
-?>
