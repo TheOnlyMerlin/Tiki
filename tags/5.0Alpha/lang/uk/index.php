23f912482f8b5b5e3a53fd70245a4b6bf3d02910
changi67 <changi67@b456876b-0849-0410-b77d-98878d47e9d5>
2010-03-12 16:19:23 +0000

    [REL] Tagging release

---
+++ tags/5.0Alpha/lang/uk/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// $Id$
+
+// This redirects to the sites root to prevent directory browsing
+
+header ("location: ../../tiki-index.php");
+die;
---
+++ tags/5.0Alpha/lib/prefs/user.php
@@ -0,0 +1,79 @@
+<?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+function prefs_user_list() {
+	return array(
+		'user_show_realnames' => array(
+			'name' => tra('Show user\'s real name instead of login (when possible)'),
+			'description' => tra('Show user\'s real name instead of login (when possible)'),
+			'help' => 'User+Preferences',
+			'type' => 'flag',
+		),
+		'user_tracker_infos' => array(
+			'name' => tra('Display UserTracker information on the user information page'),
+			'description' => tra('Display UserTracker information on the user information page'),
+			'help' => 'User+Tracker',
+			'type' => 'text',
+			'size' => '50',
+			'dependencies' => array(
+				'userTracker',
+			),
+		),
+		'user_assigned_modules' => array(
+			'name' => tra('Users can configure modules'),
+			'help' => 'Users+Configure+Modules',
+			'type' => 'flag',
+		),	
+		'user_flip_modules' => array(
+			'name' => tra('Users can shade modules'),
+			'help' => 'Users+Shade+Modules',
+			'type' => 'list',
+			'description' => tra('Allows users to hide/show modules.'),
+			'options' => array(
+				'y' => tra('Always'),
+				'module' => tra('Module decides'),
+				'n' => tra('Never'),
+			),
+		),
+		'user_store_file_gallery_picture' => array(
+			'name' => tra('Store full-size copy of avatar in file gallery'),
+			'help' => 'User+Preferences',
+			'type' => 'flag',
+		),
+		'user_picture_gallery_id' => array(
+			'name' => tra('File gallery to store full-size copy of avatar in'),
+			'description' => tra('Enter the gallery id here. Please create a dedicated gallery that is admin-only for security, or make sure gallery permissions are set so that only admins can edit.'),
+			'help' => 'User+Preferences',
+			'type' => 'text',
+			'filter' => 'digits',
+			'size' => '3',
+		),
+		'user_who_viewed_my_stuff' => array(
+			'name' => tra('Display who viewed my stuff on the user information page'),
+			'description' => tra('You will need to activate tracking of views for various items in the action log for this to work'),
+			'type' => 'flag',
+			'dependencies' => array(
+				'feature_actionlog',
+			),
+		),
+		'user_who_viewed_my_stuff_days' => array(
+			'name' => tra('Number of days to consider who viewed my stuff'),
+			'description' => tra('Number of days before current time to consider when showing who viewed my stuff'),
+			'type' => 'text',
+			'filter' => 'digit',
+			'size' => '4',
+		),
+		'user_who_viewed_my_stuff_show_others' => array(
+			'name' => tra('Show to others who viewed my stuff on the user information page'),
+			'description' => tra('Show to others who viewed my stuff on the user information page. Admins can always see this information.'),
+			'type' => 'flag',
+			'dependencies' => array(
+				'user_who_viewed_my_stuff',
+			),
+		),
+	);
+}
