5e4394c807f5193c9aec4c315d6160ebbae15dc8
changi67 <changi67@b456876b-0849-0410-b77d-98878d47e9d5>
2010-11-04 20:36:49 +0000

    [REL] Tagging release

---
+++ tags/6.0RC1/lang/ca/language.js
@@ -0,0 +1,21 @@
+/* 
+ * Javascript translation definition
+ * Example in Catalan
+ * 
+ * $Id$
+ */
+
+lang = {
+
+"Very Secure" : "Molt segura",
+"Secure" : "Segura",
+"Very Strong" : "Molt forta",
+"Strong" : "Forta",
+"Average" : "Intermitja",
+"Weak" : "Fluixa",
+"Very Weak" : "Molt fluixa",
+"Strength" : "Força",
+"Valid User Name" : "Nom d'usuari vàlid",
+"Passwords match" : "Les contrasenyes coincideixen",
+"Valid Email" : "Correu-e vàlid"
+};
