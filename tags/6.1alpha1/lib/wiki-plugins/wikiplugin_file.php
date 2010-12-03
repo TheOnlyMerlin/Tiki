b940c9696f6c0797195639afb68dc391611dc24f
changi67 <changi67@b456876b-0849-0410-b77d-98878d47e9d5>
2010-12-03 20:13:02 +0000

    [REL] Tagging release

---
+++ tags/6.1alpha1/lib/wiki-plugins/wikiplugin_file.php
@@ -0,0 +1,164 @@
+<?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+function wikiplugin_file_info()
+{
+	return array(
+		'name' => tra( 'File' ),
+		'documentation' => 'PluginFile',
+		'description' => tra("Displays a link to a file (either from the file gallery or an attachment to a wiki page) and can display an image attachment. For more than one file from file galleries, or more optional information shown from the file/s, use the plugin FILES instead"),
+		'prefs' => array( 'wikiplugin_file' ),
+		'body' => tra('Label for the link to the file'),
+		'icon' => 'pics/icons/file-manager.png',
+		'inline' => true,
+		'params' => array(
+			'type' => array(
+				'required' => true,
+				'name' => tra('Type'),
+				'description' => tra('Choose either File from gallery or Wiki page attachment.'),
+				'options' => array(
+					array('text' => tra('File from file gallery'), 'value' => 'gallery'),
+					array('text' => tra('Wiki page attachment'), 'value' => 'attachment'),
+				),
+			),
+			'name' => array(
+				'required' => true,
+				'name' => tra('Name'),
+				'description' => tra("Gives the name of the attached file to link to"),
+				'parent' => array('name' => 'type', 'value' => 'attachment'),
+			),
+ 			'desc' => array(
+				'required' => false,
+				'name' => tra('Description'),
+				'description' => tra('Used as link label'),
+				'parent' => array('name' => 'type', 'value' => 'attachment'),
+				'advanced' => true,
+			),
+			'page' => array(
+				'required' => false,
+				'name' => tra('Page'),
+				'description' => tra("Name of the wiki page the file is attached to. If left empty when the plugin is used on a wiki page, this defaults to that wiki page."),
+				'parent' => array('name' => 'type', 'value' => 'attachment'),
+				'advanced' => true,
+			),
+			'showdesc' => array(
+				'required' => false,
+				'name' => tra('Show attachment description'),
+				'description' => tra("Show the attachment description as the link label instead of the attachment name."),
+				'options' => array(
+					array('text' => tra('No'), 'value' => ''), 
+					array('text' => tra('Yes'), 'value' => 'y'),  
+				),
+				'parent' => array('name' => 'type', 'value' => 'attachment'),
+				'advanced' => true,
+			),
+			'image' =>array(
+				'required' => false,
+				'name' => tra('Image'),
+				'description' => tra("Says that this file is an image, and should be displayed inline using the img tag"),
+				'parent' => array('name' => 'type', 'value' => 'attachment'),
+				'advanced' => true,
+			),
+			'fileId' => array(
+				'required' => true,
+				'name' => tra('File identifier'),
+				'description' => tra('Identifier of a file in the file galleries.') . ' ' . tra('Example value:') . ' 42',
+				'type' => 'fileId',
+				'area' => 'fgal_picker_id',
+				'filter' => 'digits',
+				'parent' => array('name' => 'type', 'value' => 'gallery'),
+			),
+			'date' => array(
+				'required' => false,
+				'name' => tra('Date'),
+				'description' => tra('Pick the archive if exists created just before the date of the fileId'),
+				'parent' => array('name' => 'type', 'value' => 'gallery'),
+				'advanced' => true,
+			),
+			'showicon' => array(
+				'required' => false,
+				'name' => tra('Show icon'),
+				'description' => 'y|n',
+				'filter' => 'alpha',
+				'parent' => array('name' => 'type', 'value' => 'gallery'),
+				'advanced' => true,
+			),
+		)
+	);
+}
+
+function wikiplugin_file( $data, $params )
+{
+	global $tikilib, $prefs;
+	if (isset($params['fileId'])) {
+		global $filegallib; include_once ('lib/filegals/filegallib.php');
+		if ($prefs['feature_file_galleries'] != 'y') {
+			return;
+		}
+		$fileId = $params['fileId'];
+		if (isset($params['date'])) {
+			static $wikipluginFileDate = 0;
+			if (empty($params['date'])) {
+				if (empty($wikipluginFileDate)) {
+					return tra('The date has not been set');
+				}
+				$date = $wikipluginFileDate;
+			} else {
+				if (($date = strtotime($params['date'])) === false) {
+					return tra('Incorrect date format');
+				}
+				$wikipluginFileDate = $date;
+			}
+			$fileId = $filegallib->getArchiveJustBefore($fileId, $date);
+			if (empty($fileId)) {
+				return tra('No such file');
+			}
+		} else {
+			$info = $filegallib->get_file_info($fileId);
+			if (empty($info)) {
+				return tra('Incorrect param').' fileId';
+			}
+		}
+			
+		if (empty($data)) { // to avaoid problem with parsing
+			$data = empty($info['name'])?$info['filename']: $info['name'];
+		}
+		if (isset($params['showicon']) & $params['showicon'] == "y") {
+			return "{img src=tiki-download_file.php?fileId=$fileId&amp;thumbnail=y&amp;x=16 link=tiki-download_file.php?fileId=$fileId} [tiki-download_file.php?fileId=$fileId|$data]";
+		} else {
+			return "[tiki-download_file.php?fileId=$fileId|$data]";
+		}
+	}
+
+	if ($prefs['feature_wiki_attachments'] != 'y') {
+		return "<span class='warn'>" . tra("Wiki attachments are disabled."). "</span>";
+	}	
+	$filedata = array();
+	$filedata["name"] = '';
+	$filedata["desc"] = '';
+	$filedata["showdesc"] = '';
+	$filedata["page"] = '';
+	$filedata["image"] = '';
+
+	$filedata = array_merge( $filedata, $params );
+
+	if( ! $filedata["name"] ) {
+		return;
+	}
+
+	$forward = array();
+	$forward['file'] = $filedata['name'];
+	$forward['inline'] = 1;
+	$forward['page'] = $filedata['page'];
+	if($filedata['showdesc'])
+		$forward['showdesc'] = 1;
+	if($filedata['image'])
+		$forward['image'] = 1;
+	$middle = $filedata["desc"];
+
+	return $tikilib->plugin_execute( 'attach', $middle, $forward );
+}
---
+++ tags/6.1alpha1/lib/wiki-plugins/wikiplugin_flash.php
@@ -0,0 +1,124 @@
+<?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+// Wiki plugin to display a SWF file
+// damian aka damosoft 30 March 2004
+
+function wikiplugin_flash_info() {
+	return array(
+		'name' => tra('Flash video'),
+		'documentation' => 'PluginFlash',
+		'description' => tra('Displays a Flash (.swf) file in the wiki page'),
+		'prefs' => array('wikiplugin_flash'),
+		'extraparams' => true,
+		'icon' => 'pics/icons/page_white_flash.png',
+		'params' => array(
+			'type' => array(
+				'required' => true,
+				'name' => tra('Flash type'),
+				'description' => tra('Whether you want to insert a Flash from a URL, a fileId from a podcast file gallery or a link to a specific service like Youtube or Vimeo'),
+				'options' => array(
+					array('text' => tra('Select an option'), 'value' => ''),
+					array('text' => tra('Blip.tv'), 'value' => 'bliptv'), 
+					array('text' => tra('FileId from a podcast file gallery'), 'value' => 'fileId'),
+					array('text' => tra('Movie URL'), 'value' => 'url'),
+					array('text' => tra('Vimeo'), 'value' => 'vimeo'),
+					array('text' => tra('Youtube'), 'value' => 'youtube'),
+				),
+			),
+			'movie' => array(
+				'required' => true,
+				'name' => tra('Movie URL'),
+				'description' => tra('Complete URL to the movie to include. e.g. files/test.swf'),
+				'parent' => array('name' => 'type', 'value' => 'url'),
+			),
+			'fileId' => array(
+				'required' => true,
+				'name' => tra('FileId from a podcast file gallery'),
+				'description' => tra('Id of a file from a podcast gallery - will work only with podcast gallery'),
+				'parent' => array('name' => 'type', 'value' => 'fileId'),
+			),
+			'youtube' => array(
+				'required' => true,
+				'name' => tra('Youtube URL'),
+				'description' => tra('Entire URL to the YouTube video. Example: http://www.youtube.com/watch?v=1i2ZnU4iR24'),
+				'parent' => array('name' => 'type', 'value' => 'youtube'),
+			),
+			'vimeo' => array(
+				'required' => true,
+				'name' => tra('Vimeo URL'),
+				'description' => tra('Entire URL to the Vimeo video. Example: http://vimeo.com/3319966'),
+				'parent' => array('name' => 'type', 'value' => 'vimeo'),
+			),
+			'bliptv' => array(
+				'required' => true,
+				'name' => tra('Blip.tv URL'),
+				'description' => tra('Blip.tv embed URL. Example: http://blip.tv/play/AYGd_GAC'),
+				'parent' => array('name' => 'type', 'value' => 'bliptv'),
+			),
+			'width' => array(
+				'required' => false,
+				'name' => tra('Width'),
+				'description' => tra('Default width: 425'),
+				'advanced' => true,
+			),
+			'height' => array(
+				'required' => false,
+				'name' => tra('Height'),
+				'description' => tra('Default height: 350'),
+				'advanced' => true,
+			),
+			'quality' => array(
+				'required' => false,
+				'name' => tra('Quality'),
+				'description' => tra('Flash video quality. Default value: high'),
+				'advanced' => true,
+			),
+		),
+	);
+}
+
+function wikiplugin_flash($data, $params) {
+	global $tikilib, $prefs, $userlib, $user;
+	
+	// Handle file from a podcast file gallery
+	if (isset($params['fileId']) && !isset($params['movie'])) {
+		global $filegallib; include_once ('lib/filegals/filegallib.php');
+		$file_info = $filegallib->get_file_info($params['fileId']);
+		if (!$userlib->user_has_perm_on_object($user, $file_info['galleryId'], 'file gallery', 'tiki_p_view_file_gallery')) {
+			return tra('Permission denied');
+		}
+		$params['movie'] = $prefs['fgal_podcast_dir'].$file_info['path'];
+	}
+
+	// Handle Youtube video
+	if (isset($params['youtube']) && preg_match('|http(s)?://(\w+\.)?youtube\.com/watch\?v=([\w-]+)|', $params['youtube'], $matches)) {
+		$params['movie'] = "http://www.youtube.com/v/" . $matches[3];
+	}
+
+	// Handle Vimeo video
+	if (isset($params['vimeo']) && preg_match('|http(s)?://(www\.)?vimeo\.com/(clip:)?(\d+)|', $params['vimeo'], $matches)) {
+		$params['movie'] = 'http://vimeo.com/moogaloop.swf?clip_id=' . $matches[4];
+	}
+	
+	// Handle Blip.tv video
+	// We need the embed URL because there is tno relation between the video URL and the embed URL
+	if (isset($params['bliptv']) && preg_match('|http://blip.tv/play/\w+|', $params['bliptv'], $matches)) {
+		$params['movie'] = $params['bliptv'];
+	}	
+
+	if ((isset($params['youtube']) || isset($params['vimeo']) || isset($params['bliptv'])) && !isset($params['movie'])) {		
+		return tra('Invalid URL');
+	}
+		
+	$code = $tikilib->embed_flash($params);
+
+	if ( $code === false ) {
+		return tra('Missing parameter movie to the plugin flash');
+	}
+	return "~np~$code~/np~";
+}
