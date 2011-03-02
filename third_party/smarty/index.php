7a0d0e739df5c17b5d55fe8b4e2ad64abc5ca0a3
pkdille <pkdille@b456876b-0849-0410-b77d-98878d47e9d5>
2011-03-02 21:40:11 +0000

    Fix some copyright and svn props

--- third_party/smarty/index.php
+++ third_party/smarty/index.php
@@ -1,10 +1,9 @@
 <?php
-
-// $Id: /cvsroot/tikiwiki/_smarty/index.php,v 1.4.2.1 2007-11-04 22:08:35 nyloth Exp $
-
-// Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
+// 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
 
 // This redirects to the sites root to prevent directory browsing
 
