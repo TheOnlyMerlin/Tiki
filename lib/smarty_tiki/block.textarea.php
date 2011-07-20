TheOnlyMerlin/Tiki

commit 3686f0900547ae38d89e5cb10979bc17b48c1f62

NOTICE
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@35443 b456876b-0849-0410-b77d-98878d47e9d5

lib/smarty_tiki/block.textarea.php
@@ -269,7 +269,7 @@ function CKeditor_OnComplete() {
 	$js_editconfirm = '';
 	$js_editlock = '';
 
-	if ($params['_simple'] == 'n' && $params['comments'] !== 'y') {
+	if ($params['_simple'] == 'n' && (isset($params['comments']) && $params['comments'] !== 'y')) {
 // Display edit time out
 
 		$js_editlock .= "

templates/tiki-editpage.tpl
@@ -31,13 +31,13 @@
 });{/jq}
 {/if}
 {if $translation_mode eq 'n'}
-	{if $beingStaged eq 'y' and $prefs.wikiapproval_hideprefix == 'y'}{assign var=pp value=$approvedPageName}{else}{assign var=pp value=$page}{/if}
+	{if (isset($beingStaged) && $beingStaged eq 'y') and $prefs.wikiapproval_hideprefix == 'y'}{assign var=pp value=$approvedPageName}{else}{assign var=pp value=$page}{/if}
 	{title}{if isset($hdr) && $prefs.wiki_edit_section eq 'y'}{tr}Edit Section{/tr}{else}{tr}Edit{/tr}{/if}: {$pp}{if $pageAlias ne ''}&nbsp;({$pageAlias}){/if}{/title}
 {else}
    {title}{tr}Update '{$page}'{/tr}{/title}
 {/if}
    
-{if $beingStaged eq 'y'}
+{if isset($beingStaged) && $beingStaged eq 'y'}
 	<div class="tocnav">{icon _id=information style="vertical-align:middle" align="left"} 
 		{if $approvedPageExists}
 			{tr}You are editing the staging copy of the approved version of this page. Changes will be merged in after approval.{/tr}
@@ -52,7 +52,7 @@
 		{/if}
 	</div>
 {/if}
-{if $needsStaging eq 'y'}
+{if isset($needsStaging) && $needsStaging eq 'y'}
 	<div class="tocnav">
 		{icon _id=information style="vertical-align:middle" align="left"} 
 		{tr}You are editing the approved copy of this page.{/tr}
@@ -83,7 +83,7 @@
 	<div class="highlight"><em class='mandatory_note'>{tr}A contribution is mandatory{/tr}</em></div>
 	{/remarksbox}
 {/if}
-{if $summary_needed eq 'y'}
+{if isset($summary_needed) && $summary_needed eq 'y'}
 	{remarksbox type='Warning' title="{tr}Warning{/tr}"}
 	<div class="highlight"><em class='mandatory_note'>{tr}An edit summary is mandatory{/tr}</em></div>
 	{/remarksbox}
@@ -113,7 +113,7 @@
 {if $preview or $prefs.ajax_autosave eq "y"}
 	{include file='tiki-preview.tpl'}
 {/if}
-{if $diff_style}
+{if isset($diff_style)}
 	<div id="diff_outer">
 		<div style="overflow:auto;height:20ex;" id="diff_history">
 			{if $translation_mode == 'y'}		
@@ -148,11 +148,11 @@
 
 	<input type="hidden" name="no_bl" value="y" />
 	{if !empty($smarty.request.returnto)}<input type="hidden" name="returnto" value="{$smarty.request.returnto}" />{/if}
-	{if $diff_style}
+	{if isset($diff_style)}
 		<select name="diff_style" class="wikiaction"title="{tr}Edit wiki page{/tr}|{tr}Select the style used to display differences to be translated.{/tr}">
-			<option value="htmldiff"{if $diff_style eq "htmldiff"} selected="selected"{/if}>{tr}html{/tr}</option>
-			<option value="inlinediff"{if $diff_style eq "inlinediff"} selected="selected"{/if} >{tr}text{/tr}</option>			  
-			<option value="inlinediff-full"{if $diff_style eq "inlinediff-full"} selected="selected"{/if} >{tr}text full{/tr}</option>			  
+			<option value="htmldiff"{if isset($diff_style) && $diff_style eq "htmldiff"} selected="selected"{/if}>{tr}html{/tr}</option>
+			<option value="inlinediff"{if isset($diff_style) && $diff_style eq "inlinediff"} selected="selected"{/if} >{tr}text{/tr}</option>			  
+			<option value="inlinediff-full"{if isset($diff_style) && $diff_style eq "inlinediff-full"} selected="selected"{/if} >{tr}text full{/tr}</option>			  
 		</select>
 		<input type="submit" class="wikiaction tips" title="{tr}Edit wiki page{/tr}|{tr}Change the style used to display differences to be translated.{/tr}" name="preview" value="{tr}Change diff styles{/tr}" onclick="needToConfirm=false;" />
 	{/if}
@@ -201,7 +201,7 @@
 							<fieldset>
 								<label for="comment">{tr}Describe the change you made:{/tr} {help url='Editing+Wiki+Pages' desc="{tr}Edit comment: Enter some text to describe the changes you are currently making{/tr}"}</label>
 								<input style="width:98%;" class="wikiedit" type="text" id="comment" name="comment" value="{$commentdata|escape}" />
-								{if $show_watch eq 'y'}
+								{if isset($show_watch) && $show_watch eq 'y'}
 									<label for="watch">{tr}Monitor this page:{/tr}</label>
 									<input type="checkbox" id="watch" name="watch" value="1"{if $watch_checked eq 'y'} checked="checked"{/if} />
 								{/if}
@@ -214,7 +214,7 @@
 									</table>
 								</fieldset>
 							{/if}
-							{if $wysiwyg neq 'y' and $prefs.feature_wiki_pictures eq 'y' and $tiki_p_upload_picture eq 'y' and $prefs.feature_filegals_manager neq 'y'}
+							{if (isset($wysiwyg) && $wysiwyg neq 'y') and $prefs.feature_wiki_pictures eq 'y' and $tiki_p_upload_picture eq 'y' and $prefs.feature_filegals_manager neq 'y'}
 								<fieldset>
 									<legend>{tr}Upload picture:{/tr}</legend>
 									<input type="hidden" name="MAX_FILE_SIZE" value="1000000000" />
@@ -310,7 +310,7 @@ $("#allowhtml").change(function() {
 								</fieldset>
 							{/if}
 							
-							{if $wysiwyg neq 'y'}
+							{if isset($wysiwyg) && $wysiwyg neq 'y'}
 								{if $prefs.feature_wiki_attachments == 'y' and ($tiki_p_wiki_attach_files eq 'y' or $tiki_p_wiki_admin_attachments eq 'y')}
 									<fieldset>
 										<legend>{tr}Upload file:{/tr}</legend>
@@ -507,7 +507,7 @@ $("#allowhtml").change(function() {
 		
 		
 		{if $page|lower ne 'sandbox'}
-			{if $prefs.feature_antibot eq 'y' && $anon_user eq 'y'}
+			{if $prefs.feature_antibot eq 'y' && (isset($anon_user) && $anon_user eq 'y')}
 				{include file='antibot.tpl' tr_style="formcolor"}
 			{/if}
 		{/if}{* sandbox *}

templates/tiki-preview.tpl
@@ -4,8 +4,8 @@
 	<div style="float:right;">
 		<select name="diff_style" id="preview_diff_style">
 			<option value="" {if empty($diff_style)}selected="selected"{/if}>{tr}Preview{/tr}</option>
-			<option value="htmldiff" {if $diff_style == "htmldiff"}selected="selected"{/if}>{tr}HTML diff{/tr}</option>
-			<option value="sidediff" {if $diff_style == "sidediff"}selected="selected"{/if}>{tr}Side-by-side diff{/tr}</option>
+			<option value="htmldiff" {if isset($diff_style) && $diff_style == "htmldiff"}selected="selected"{/if}>{tr}HTML diff{/tr}</option>
+			<option value="sidediff" {if isset($diff_style) && $diff_style == "sidediff"}selected="selected"{/if}>{tr}Side-by-side diff{/tr}</option>
 		</select>
 		{jq}
 $("#preview_diff_style").change(function(){
@@ -37,11 +37,11 @@ $('#autosave_preview').resizable({
 }).height(getCookie("wiki", "preview", ""));
 $("#autosave_preview>div").height(getCookie("wiki", "preview", ""));
 {/jq}{/if}
-<h2>{tr}Preview{/tr} {if $staging_preview eq 'y'}of current staging copy{/if}: {if $beingStaged eq 'y' and $prefs.wikiapproval_hideprefix == 'y'}{$approvedPageName|escape}{else}{$page|escape}{/if}</h2>
+<h2>{tr}Preview{/tr} {if isset($staging_preview) && $staging_preview eq 'y'}of current staging copy{/if}: {if isset($beingStaged) && $beingStaged eq 'y' and $prefs.wikiapproval_hideprefix == 'y'}{$approvedPageName|escape}{else}{$page|escape}{/if}</h2>
 {if $prefs.feature_wiki_description eq 'y'}
 <small>{$description}</small>
 {/if}
-{if $staging_preview neq 'y'}
+{if isset($staging_preview) && $staging_preview neq 'y'}
 <div align="center" class="attention" style="font-weight:bold">{tr}Note: Remember that this is only a preview, and has not yet been saved!{/tr}</div>
 {/if}
 <div  class="wikitext">

templates/tiki-view_tracker_item.tpl
@@ -1,7 +1,7 @@
 {* $Id$ *}
 {title help="trackers"}{$tracker_info.name}{/title}
 
-{if $print_page ne 'y'}
+{if isset($print_page) && $print_page ne 'y'}
 
 	{* --------- navigation ------ *}
 	<div class="navbar">
@@ -111,7 +111,7 @@
 {/if}
 
 {* --------------------------------------------------------------- tab with edit --- *}
-{if $print_page ne 'y' && ($tiki_p_modify_tracker_items eq 'y' and $item_info.status ne 'p' and $item_info.status ne 'c') or ($tiki_p_modify_tracker_items_pending eq 'y' and $item_info.status eq 'p') or ($tiki_p_modify_tracker_items_closed eq 'y' and $item_info.status eq 'c') or $special}
+{if (isset($print_page) && $print_page ne 'y') && ($tiki_p_modify_tracker_items eq 'y' and $item_info.status ne 'p' and $item_info.status ne 'c') or ($tiki_p_modify_tracker_items_pending eq 'y' and $item_info.status eq 'p') or ($tiki_p_modify_tracker_items_closed eq 'y' and $item_info.status eq 'c') or $special}
 	{capture name="editTitle"}{if ($tiki_p_remove_tracker_items eq 'y' and $item_info.status ne 'p' and $item_info.status ne 'c') or ($tiki_p_remove_tracker_items_pending eq 'y' and $item_info.status eq 'p') or ($tiki_p_remove_tracker_items_closed eq 'y' and $item_info.status eq 'c')}{tr}Edit/Delete{/tr}{else}{tr}Edit{/tr}{/if}{/capture}
 	{tab name=$smarty.capture.editTitle}
 		<h2>{tr}Edit Item{/tr}</h2>
@@ -288,7 +288,7 @@
 {/tabset}
 <br /><br />
 
-{if $print_page eq 'y'}
+{if isset($print_page) && $print_page eq 'y'}
 	{capture name=url}{$base_url}{$itemId|sefurl:trackeritem}{/capture}
 	{tr}The original document is available at{/tr} <a href="{$smarty.capture.url}">{$smarty.capture.url}</a>
 {/if}

templates/tiki-wiki_staging.tpl
@@ -1,5 +1,5 @@
 {* $Id$ *}
-{if $beingStaged eq 'y'}
+{if isset($beingStaged) && $beingStaged eq 'y'}
 <div class="tocnav">
 {tr}This is the staging copy of{/tr} <a class="link" href="tiki-index.php?page={$approvedPageName|escape:'url'}">{tr}the approved version of this page.{/tr}</a>
 {if $outOfSync eq 'y'}

templates/tiki-wiki_topline.tpl
@@ -18,7 +18,7 @@
 				{/if}
 			</div>
 
-			{if not $versioned}
+			{if !isset($versioned)}
 				{if $print_page ne 'y'}
 					<div class="wikiactions" style="float: right; padding-left:10px; white-space: nowrap">
 						<div class="icons" style="float: left;">
@@ -27,7 +27,7 @@
 							{/if}
 							{if $prefs.flaggedrev_approval neq 'y' or ! $revision_approval or $lastVersion eq $revision_displayed}
 								{if $editable and ($tiki_p_edit eq 'y' or $page|lower eq 'sandbox') and $beingEdited ne 'y' and $machine_translate_to_lang eq '' or $canEditStaging eq 'y'}
-									<a title="{tr}Edit this page{/tr}" {ajax_href template="tiki-editpage.tpl"}tiki-editpage.php?page={if $needsStaging eq 'y'}{$stagingPageName|escape:"url"}{else}{$page|escape:"url"}{/if}{if !empty($page_ref_id) and $needsStaging neq 'y'}&amp;page_ref_id={$page_ref_id}{/if}{/ajax_href}>{icon _id='page_edit' alt="{tr}Edit this page{/tr}"}</a>
+									<a title="{tr}Edit this page{/tr}" {ajax_href template="tiki-editpage.tpl"}tiki-editpage.php?page={if isset($needsStaging) && $needsStaging eq 'y'}{$stagingPageName|escape:"url"}{else}{$page|escape:"url"}{/if}{if !empty($page_ref_id) and $needsStaging neq 'y'}&amp;page_ref_id={$page_ref_id}{/if}{/ajax_href}>{icon _id='page_edit' alt="{tr}Edit this page{/tr}"}</a>
 									{if $prefs.wiki_edit_icons_toggle eq 'y' and ($prefs.wiki_edit_plugin eq 'y' or $prefs.wiki_edit_section eq 'y')}
 										{jq}
 											$("#wiki_plugin_edit_view").click( function () {

templates/wiki_edit_actions.tpl
@@ -12,7 +12,7 @@ return false;
 });{/jq}
 {/if}
 {if $page|lower neq 'sandbox' or $tiki_p_admin eq 'y'}
-	{if ! $page_badchars_display or $prefs.wiki_badchar_prevent neq 'y'}
+	{if ! isset($page_badchars_display) or $prefs.wiki_badchar_prevent neq 'y'}
 		{if $translation_mode eq 'y'}
 			<input type="hidden" name="source_page" value="{$source_page|escape}"/>
 			<input type="hidden" name="target_page" value="{$target_page|escape}"/>

commit 3682b6347577ea713fbf016ee51ff9f47c95d436

[FIX] textarea: Fix wide reaching loss of textarea functionality caused by boolean misunderstandings in r35443 (by setting a sensible default at the start of the file). Minor change of behaviour possible due to removal of link between comments and _simple params which shouldn't have been there in the first place.

lib/smarty_tiki/block.textarea.php
@@ -48,6 +48,7 @@ function smarty_block_textarea($params, $content, &$smarty, $repeat) {
 	$params['id'] = isset($params['id']) ? $params['id'] : 'editwiki';
 	$params['area_id'] = isset($params['area_id']) ? $params['area_id'] : $params['id'];	// legacy param for toolbars?
 	$params['class'] = isset($params['class']) ? $params['class'] : 'wikiedit';
+	$params['comments'] = isset($params['comments']) ? $params['comments'] : 'n';
 
 	//codemirror integration
 	if ($prefs['feature_syntax_highlighter'] === 'y') {
@@ -255,7 +256,7 @@ function CKeditor_OnComplete() {
 			$smarty->assign('textarea_attributes', $textarea_attributes);
 		}
 		$smarty->assign_by_ref('pagedata', htmlspecialchars($content));
-		$smarty->assign('comments', isset($params['comments']) ? $params['comments'] : $params['_simple'] === 'y' ? 'y' : 'n');
+		$smarty->assign('comments', $params['comments']);	// jb removed fallback to using _simple here if comments not set 110720
 		$smarty->assign('switcheditor', isset($params['switcheditor']) ? $params['switcheditor'] : 'n');
 		$smarty->assign('toolbar_section', $params['section']);
 		$html .= $smarty->fetch('wiki_edit.tpl');
@@ -269,7 +270,7 @@ function CKeditor_OnComplete() {
 	$js_editconfirm = '';
 	$js_editlock = '';
 
-	if ($params['_simple'] == 'n' && (isset($params['comments']) && $params['comments'] !== 'y')) {
+	if ($params['_simple'] == 'n' && $params['comments'] !== 'y') {
 // Display edit time out
 
 		$js_editlock .= "