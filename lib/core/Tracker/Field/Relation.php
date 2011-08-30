TheOnlyMerlin/Tiki

commit bd30f77408dd6d8104ac6b7fdc846792d0a70d7c

[ENH] Replace direct overrides of context values to pass them to the template
git-svn-id: file:///var/local/code.tiki.org/tikisvn/trunk@36496 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Tracker/Field/Relation.php
@@ -91,17 +91,19 @@ class Tracker_Field_Relation extends Tracker_Field_Abstract
 			return tra('Read-only');
 		}
 
-		$context['labels'] = array();
+		$labels = array();
 		foreach ($this->getConfiguration('relations') as $rel) {
 			list($type, $id) = explode(':', $rel, 2);
-			$context['labels'][$rel] = TikiLib::lib('object')->get_title($type, $id);
+			$labels[$rel] = TikiLib::lib('object')->get_title($type, $id);
 		}
 		foreach ($this->getConfiguration('inverts') as $rel) {
 			list($type, $id) = explode(':', $rel, 2);
-			$context['labels'][$rel] = TikiLib::lib('object')->get_title($type, $id);
+			$labels[$rel] = TikiLib::lib('object')->get_title($type, $id);
 		}
-		$context['filter'] = $this->buildFilter();
-		return $this->renderTemplate('trackerinput/relation.tpl', $context);
+		$labels = $this->buildFilter();
+		return $this->renderTemplate('trackerinput/relation.tpl', $context, array(
+			'labels' => $labels,
+		));
 	}
 
 	function renderOutput($context = array())

lib/core/Tracker/Field/Category.php
@@ -115,18 +115,16 @@ class Tracker_Field_Category extends Tracker_Field_Abstract implements Tracker_F
 	{
 		$selected_categories = $this->getConfiguration('selected_categories');
 		$categories = $this->getConfiguration('list');
-		$ret = '';
+		$ret = array();
 		foreach ($selected_categories as $categId) {
-			if (!empty($ret))
-				$ret .= '<br />';
 			foreach ($categories as $category) {
 				if ($category['categId'] == $categId) {
-					$ret .= $category['name'];
+					$ret[] = $category['name'];
 					break;
 				}
 			}
 		}
-		return $ret;
+		return implode('<br/>', $ret);
 	}
 
 	function handleSave($value, $oldValue)

lib/core/Tracker/Field/DateTime.php
@@ -85,20 +85,22 @@ class Tracker_Field_DateTime extends Tracker_Field_Abstract implements Tracker_F
 			$date = $tikilib->get_short_date($value);
 			if ($this->getOption(0) == 'd') {
 				return $date;
-			} elseif ($this->getOption(0) == 't') {
+			}
+			
+			if ($this->getOption(0) == 't') {
+				return $tikilib->get_short_time($value);
+			}
+
+			if ($context['list_mode'] == 'csv') {
+				return $tikilib->get_short_datetime($value, false);
+			}
+
+			$current = $tikilib->get_short_date($tikilib->now);
+
+			if ($date == $current) {
 				return $tikilib->get_short_time($value);
 			} else {
-				if ($context['list_mode'] == 'csv') {
-					return $tikilib->get_short_datetime($value, false);
-				} else {
-					$current = $tikilib->get_short_date($tikilib->now);
-
-					if ($date == $current) {
-						return $tikilib->get_short_time($value);
-					} else {
-						return $tikilib->get_short_datetime($value, false);
-					}
-				}
+				return $tikilib->get_short_datetime($value, false);
 			}
 		}
 	}

lib/core/Tracker/Field/File.php
@@ -69,27 +69,28 @@ class Tracker_Field_File extends Tracker_Field_Abstract
 
 		if (empty($att_id)) {
 			return '';
-		} else {
-
-			if ($context['list_mode'] === 'csv') {
-				global $base_url;
-				return $base_url . 'tiki-download_item_attachment.php?attId=' . $att_id;	// should something to do with export_attachment() happen here?
-			}
-
-			$attachment = TikiLib::lib('trk')->get_item_attachment($att_id);
-
-			$smarty = TikiLib::lib('smarty');
-			$smarty->loadPlugin('smarty_block_self_link');
-			$smarty->loadPlugin('smarty_function_icon');
-
-			$link = smarty_block_self_link(array(
-												'_script' => 'tiki-download_item_attachment.php',
-												'attId' => $att_id,
-										   ),
-										   smarty_function_icon(array('_id' => 'disk', 'alt' => tra('Download')), $smarty) . ' ' .
-										   $attachment['filename'],
-										   $smarty);
 		}
+
+		if ($context['list_mode'] === 'csv') {
+			global $base_url;
+			return $base_url . 'tiki-download_item_attachment.php?attId=' . $att_id;	// should something to do with export_attachment() happen here?
+		}
+
+		$attachment = TikiLib::lib('trk')->get_item_attachment($att_id);
+
+		$smarty = TikiLib::lib('smarty');
+		$smarty->loadPlugin('smarty_block_self_link');
+		$smarty->loadPlugin('smarty_function_icon');
+
+		$link = smarty_block_self_link(
+			array(
+				'_script' => 'tiki-download_item_attachment.php',
+				'attId' => $att_id,
+			),
+			smarty_function_icon(array('_id' => 'disk', 'alt' => tra('Download')), $smarty) . ' ' .
+			$attachment['filename'],
+			$smarty
+		);
 		return $link;
 	}
 

lib/core/Tracker/Field/Image.php
@@ -166,8 +166,9 @@ class Tracker_field_Image extends Tracker_Field_File
 
 	function renderInput($context = array())
 	{
-		$context['image_tag'] = $this->renderInnerOutput($context);
-		return $this->renderTemplate('trackerinput/image.tpl', $context);
+		return $this->renderTemplate('trackerinput/image.tpl', $context, array(
+			'image_tag' => $this->renderInnerOutput($context),
+		));
 	}
 
 	function handleSave($value, $oldValue)

lib/core/Tracker/Field/Language.php
@@ -51,8 +51,9 @@ class Tracker_Field_Language extends Tracker_Field_Abstract implements Tracker_F
 
 	function renderInput($context = array())
 	{
-		$context['languages'] = $this->getLanguages();
-		return $this->renderTemplate('trackerinput/language.tpl', $context);
+		return $this->renderTemplate('trackerinput/language.tpl', $context, array(
+			'languages' => $this->getLanguages(),
+		));
 	}
 
 	function renderOutput($context = array())
@@ -60,8 +61,9 @@ class Tracker_Field_Language extends Tracker_Field_Abstract implements Tracker_F
 		$selected = $this->getConfiguration('value');
 
 		$languages = $this->getLanguages();
-		$context['label'] = isset($languages[$selected]) ? $languages[$selected] : tr('None');
-		return $this->renderTemplate('trackeroutput/language.tpl', $context);
+		return $this->renderTemplate('trackeroutput/language.tpl', $context, array(
+			'label' => isset($languages[$selected]) ? $languages[$selected] : tr('None'),
+		));
 	}
 
 	function handleSave($value, $oldValue)

lib/core/Tracker/Field/TextArea.php
@@ -92,17 +92,19 @@ class Tracker_Field_TextArea extends Tracker_Field_Text
 		$cols = $this->getOption(1);
 		$rows = $this->getOption(2);
 
-		$context['toolbar'] = $this->getOption(0) ? 'y' : 'n';
-		$context['cols'] = ($cols >= 1) ? $cols : 50;
-		$context['rows'] = ($rows >= 1) ? $rows : 50;
-		$context['keyup'] = '';
+		$data = array(
+			'toolbar' => $this->getOption(0) ? 'y' : 'n',
+			'cols' => ($cols >= 1) ? $cols : 50,
+			'rows' => ($rows >= 1) ? $rows : 50,
+			'keyup' => '',
+		);
 
 		if ($this->getOption(5)) {
-			$context['keyup'] = "wordCount({$this->getOption(5)}, this, 'cpt_{$this->getConfiguration('fieldId')}', '" . tr('Word Limit Exceeded') . "')";
+			$data['keyup'] = "wordCount({$this->getOption(5)}, this, 'cpt_{$this->getConfiguration('fieldId')}', '" . tr('Word Limit Exceeded') . "')";
 		} elseif ($this->getOption(3)) {
-			$context['keyup'] = "charCount({$this->getOption(3)}, this, 'cpt_{$this->getConfiguration('fieldId')}', '" . tr('Character Limit Exceeded') . "')";
+			$data['keyup'] = "charCount({$this->getOption(3)}, this, 'cpt_{$this->getConfiguration('fieldId')}', '" . tr('Character Limit Exceeded') . "')";
 		}
-		return $this->renderTemplate('trackerinput/textarea.tpl', $context);
+		return $this->renderTemplate('trackerinput/textarea.tpl', $context, $data);
 	}
 
 	protected function attemptParse($text)

templates/trackerinput/image.tpl
@@ -1,7 +1,7 @@
 <input type="file" name="{$field.ins_id}"{if isset($input_err)} value="{$field.value}"{/if} />
 {if $field.value ne ''}
 	<br />
-	{if $context.image_tag}{$context.image_tag}{/if}
+	{$data.image_tag}
 	{if $field.isMandatory ne 'y'}
 		<a href="{$smarty.server.PHP_SELF}?{query removeImage='y' fieldId=$field.fieldId itemId=$item.itemId trackerId=$item.trackerId fieldName=$field.name}" class="trkRemoveImage">{icon _id='cross' alt="{tr}Remove Image{/tr}"}</a>
 		{jq}$(".trkRemoveImage").click(function(){return confirm("{tr}Are you sure you want to delete this image?{/tr}");});{/jq}

commit 339a34a7d328263a4765eb5f93ae920a2dfcfacc

[FIX] Obvious breakage after r36496

lib/core/Tracker/Field/Relation.php
@@ -103,9 +103,12 @@ class Tracker_Field_Relation extends Tracker_Field_Abstract
 			list($type, $id) = explode(':', $rel, 2);
 			$labels[$rel] = TikiLib::lib('object')->get_title($type, $id);
 		}
-		$labels = $this->buildFilter();
+
+		$filter = $this->buildFilter();
+
 		return $this->renderTemplate('trackerinput/relation.tpl', $context, array(
 			'labels' => $labels,
+			'filter' => $filter,
 		));
 	}
 