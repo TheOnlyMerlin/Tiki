TheOnlyMerlin/Tiki

commit 35e0d52cdae7e1c0860a70a054d3c88963fa65ac

[FIX] Fix issues with adding availability of all child categories for trackers started with r27935 and deploy better and fix a couple of pre-existing related issues. Wound up being a bigger change than I expected.
git-svn-id: file:///var/local/code.tiki.org/tikisvn/branches/6.x@29936 b456876b-0849-0410-b77d-98878d47e9d5

lib/wiki-plugins/wikiplugin_trackerfilter.php
@@ -370,7 +370,12 @@ function wikiplugin_trackerFilter_get_filters($trackerId=0, $listfields='', &$fo
 			switch ($field['type']){
 			case 'e':// category
 				global $categlib; include_once('lib/categories/categlib.php');
-				$res = $categlib->get_child_categories($field['options_array'][0]);
+				if (isset($fopt['options_array'][3]) && $fopt['options_array'][3] == 1) {
+					$all_descends = true;
+				} else {
+					$all_descends = false;
+				}
+				$res = $categlib->get_child_categories($field['options_array'][0], $all_descends);
 				$formats[$fieldId] = (count($res) >= 6)? 'd': 'r';
 				break;
 			case 'd': // drop down list

lib/categories/categlib.php
@@ -25,8 +25,9 @@ class CategLib extends ObjectLib
 		"categpath" is a string representing the path to the category in the category tree, ordered from the ancestor to the category. Each category is separated by "::". For example, "Tiki" could have categpath "Software::Free software::Tiki". If a category is given, it is considered the root of the category tree for building categpath.
 		"tepath" is an array representing the path to the category in the category tree, ordered from the ancestor to the category. Each element is the name of the represented category.
 		"children" is the number of categories the category has as children.
-		"objects" is the number of objects directly in the category. */
-	function list_categs($categId=0, $showWS=false) {
+		"objects" is the number of objects directly in the category. 
+	If $all is set to false, only first level children obtained. */
+	function list_categs($categId=0, $showWS=false, $all = true) {
 		$back = $this->get_all_categories_ext($showWS);
 
 		if ($categId > 0) {
@@ -35,7 +36,7 @@ class CategLib extends ObjectLib
 			foreach ($back as $cat) {
 				if ($cat['categId'] == $categId)
 					$path = $cat['categpath'].'::';
-				else if ($path != '' && strpos($cat['categpath'], $path) === 0) {
+				else if (($all == true || $cat['parentId'] == $categId) && ($path != '' && strpos($cat['categpath'], $path) === 0)) {
 					$cat['categpath'] = substr($cat['categpath'], strlen($path));
 					$back2[] = $cat;
 				}
@@ -45,7 +46,40 @@ class CategLib extends ObjectLib
 			return $back;
 		}
 	}
-	
+	/* Similar to list_categs, but gets info for the category ids themselves, not descendants
+	 * $categIds can be an array.
+	 * Specifiy a common ancestor category ID in $top to remove the top level from the category path
+	 */
+	function get_category_info($categIds, $showWS=false, $top=null) {
+		$back = $this->get_all_categories_ext($showWS);
+		$i = 0;
+		$cut = '';
+		foreach ($back as $cat) {
+			$catkey = $cat['categId'];
+			if (isset($top)) {
+				if ($top == $cat['categId']) {
+					$cut = $cat['categpath'].'::';
+				} elseif ($cut != '' && strpos($cat['categpath'], $cut) === 0){
+					$cat['categpath'] = substr($cat['categpath'], strlen($cut));
+				}
+			}
+			$catlist["$catkey"] = $cat;
+			$catlist["$catkey"]['order'] = $i;
+			$i++;
+		}
+		if (is_array($categIds)) {
+			foreach ($categIds as $ids) {
+				$order = $catlist["$ids"]['order'];
+				$catinfo[$order] = $catlist["$ids"];
+			}
+			ksort($catinfo);
+			$catinfo = array_values($catinfo);
+		} else {
+			$catinfo[0] = $catlist["$categIds"];
+		}
+		return $catinfo;
+	}
+
 	function list_all_categories($offset, $maxRecords, $sort_mode = 'name_asc', $find, $type, $objid, $showWS = false, $listOnlyWS = false) {
 		$cats = $this->get_object_categories($type, $objid);
 
@@ -869,6 +903,10 @@ class CategLib extends ObjectLib
 	}
 
 	// FUNCTIONS TO CATEGORIZE SPECIFIC OBJECTS END ////
+	
+	/*Set $all_descends to true to get all descendent categories, otherwise only first level children
+	 * Should consider combining with list_categs
+	 */
 	function get_child_categories($categId, $all_descends = false) {
 		global $cachelib; include_once('lib/cache/cachelib.php');
 		global $prefs;
@@ -879,34 +917,7 @@ class CategLib extends ObjectLib
 			$cachekey = "childcategs$categId";
 		}
 		if( ! $ret = $cachelib->getSerialized("$cachekey") ) {
-			if ($all_descends == true) {
-				//find length of $categId name to delete later
-				$name = $this->get_category_name($categId);
-				$cut = strlen($name) +2;
-				$ids_array = $this->get_category_descendants($categId);
-				$length = count($ids_array);
-				$ids_array = array_slice($ids_array, 1, $length, true);
-				$ids_string = implode(", ", $ids_array); 
-				$query = "select * from `tiki_categories` where `categId` in (" . $ids_string . ") order by name";
-				$bindvars = array();		
-			} else {
-				$query = "select * from `tiki_categories` where `parentId`=? order by name";
-				$bindvars = array($categId);
-			}
-			$ret = $this->fetchAll($query,$bindvars);
-			foreach ( $ret as &$res ) {
-				$id = $res["categId"];
-				$query = "select count(*) from `tiki_categories` where `parentId`=?";
-				$res["children"] = $this->getOne($query,array($id));
-				$query = "select count(*) from `tiki_category_objects` where `categId`=?";
-				$res["objects"] = $this->getOne($query,array($id));
-				if ($all_descends == true) {
-					$res['name'] = $this->get_category_path_string($id);
-					$res['name'] = substr($res['name'], $cut);
-				} else {
-					$res['name']=$this->get_category_name($id);
-				}
-			}
+			$ret = $this->list_categs($categId, false, $all_descends);
 			$cachelib->cacheItem($cachekey,serialize($ret));
 		}
 		if ($prefs['feature_multilingual'] == 'y' && $prefs['language'] != 'en') {
@@ -916,13 +927,8 @@ class CategLib extends ObjectLib
 		}
 		return $ret;
 	}
-	//set $all_descends to true to get all descendent categories, otherwise only first level children
 	function get_viewable_child_categories($categId, $all_descends = false) {
-		if ($all_descends == true) {
-			$alls = $this->get_child_categories($categId, true);
-		} else {
-			$alls = $this->get_child_categories($categId);
-		}
+		$alls = $this->get_child_categories($categId, $all_descends);
 		if (empty($alls)) {
 			return $alls;
 		}

lib/trackers/trackerlib.php
@@ -1073,23 +1073,15 @@ class TrackerLib extends TikiLib
 				$this->update_star_field($trackerId, $itemId, $fopt);
 				break;
 			case 'e':
+				//affects plugin trackerlist and tiki-view_tracker display of category for each item
 				global $categlib;
 				include_once('lib/categories/categlib.php');
-				$mycats = $categlib->get_child_categories($fopt['options'], true);
-				if (empty($zcatItemId) || $zcatItemId != $itemId) {
-					$zcatItemId = $itemId;
-					$zcats = $categlib->get_object_categories('trackeritem', $itemId);
-				}
-				$cats = array();
-				$catIds = array();
-				foreach ( $mycats as $m ) {
-					if ( in_array($m['categId'], $zcats) ) {
-						$cats[] = $m;
-						$catIds[] = $m['categId'];
-					}
-				}
-				$fopt['categs'] = $cats;
-				$fopt['value'] = $catIds;
+				$itemcats = $categlib->get_object_categories('trackeritem', $itemId);
+				$itemcat = $itemcats[0];
+				$top = $fopt['options_array'][0];
+				$fopt['categs'] = $categlib->get_category_info($itemcat, false, $top);
+				unset ($top);
+				$fopt['value'] = $itemcat;
 				break;
 			case 'l':
 				if ( isset($fopt['options_array'][2]) && isset($fopt['options_array'][2]) && isset($fopt['options_array'][3])) {
@@ -1936,7 +1928,7 @@ class TrackerLib extends TikiLib
 			if (empty($trackersync_user)) {
 				$trackersync_user = $user;
 			}
-			$sig_catids = $categlib->get_category_descendants($prefs['user_trackersync_parentgroup'], true);
+			$sig_catids = $categlib->get_category_descendants($prefs['user_trackersync_parentgroup']);
 			$sig_add = array_intersect($sig_catids, $new_categs);
 			$sig_del = array_intersect($sig_catids, $del_categs);
 			$groupList = $userlib->list_all_groups();
@@ -4183,7 +4175,8 @@ class TrackerLib extends TikiLib
 		$all = $this->fetchAll($query, $bindvars, -1, 0);
 		foreach ($all as $f) {
 			if (!empty($item_categs) && $f['type'] == 'e') {//category
-				$field_categs = $categlib->get_child_categories($f['options']);
+				$f['options_array'] = explode(',',$f['options']);
+				$field_categs = $categlib->get_child_categories($f['options_array'][0], true);
 				$aux = array();
 				foreach ($field_categs as $cat) {
 					$aux[] = $cat['categId'];

lib/wiki-plugins/wikiplugin_tracker.php
@@ -1012,7 +1012,12 @@ function wikiplugin_tracker($data, $params)
 						}
 					} elseif ($f['type'] == 'e') {
 						global $categlib; include_once('lib/categories/categlib.php');
-						$flds['data'][$i]['list'] = $categlib->get_viewable_child_categories($f["options_array"][0], true);
+						if (isset($f['options_array'][3]) && $f['options_array'][3] ==1) {
+							$all_descends = true;
+						} else {
+							$all_descends = false;
+						}
+						$flds['data'][$i]['list'] = $categlib->get_viewable_child_categories($f['options_array'][0], $all_descends);
 					} elseif ($f['type'] == 'A') {
 						if (!empty($f['value'])) {
 							$flds['data'][$i]['info'] = $trklib->get_item_attachment($f['value']);

templates/tiki-view_tracker.tpl
@@ -343,7 +343,7 @@ $("#newItemForm").validate({
 	{/if}
 	{foreach key=ku item=iu from=$field_value.categories name=foreache}
 	{assign var=fcat value=$iu.categId }
-	<option value="{$iu.categId}"{if $field_value.cat.$fcat eq 'y'} selected="selected"{/if}>{$iu.name|escape}</option>
+	<option value="{$iu.categId}"{if $field_value.cat.$fcat eq 'y'} selected="selected"{/if}>{$iu.categpath|escape}</option>
 	{/foreach}
 	</select>
 {else}

templates/tiki-view_tracker_item.tpl
@@ -367,7 +367,7 @@ $("#user_selector_{{$cur_field.id}}").tiki("autocomplete", "username", {mustMatc
 	{if $cur_field.isMandatory ne 'y'}<option value=""></option>{/if}
 	{foreach key=ku item=iu from=$cur_field.$fca name=foreache}
 	{assign var=fcat value=$iu.categId }
-	<option value="{$iu.categId}"{if $cur_field.cat.$fcat eq 'y'} selected="selected"{/if}>{$iu.name|escape}</option>
+	<option value="{$iu.categId}"{if $cur_field.cat.$fcat eq 'y'} selected="selected"{/if}>{$iu.categpath|escape}</option>
 	{/foreach}
 	</select>
 {else}

templates/tracker_item_field_input.tpl
@@ -110,7 +110,7 @@
 	   		<option value=""></option>
 		{/if}
 		{foreach key=ku item=cat from=$field_value.list}
-			<option value="{$cat.categId}"{if (!is_array($field_value.value) and $field_value.value eq $cat.categId) or (is_array($field_value.value) and in_array($cat.categId, $field_value.value))} selected="selected"{/if}>{$cat.name|escape}</option>
+			<option value="{$cat.categId}"{if (!is_array($field_value.value) and $field_value.value eq $cat.categId) or (is_array($field_value.value) and in_array($cat.categId, $field_value.value))} selected="selected"{/if}>{$cat.categpath|escape}</option>
 		{/foreach}
 		</select>
 	{else}

templates/tracker_item_field_value.tpl
@@ -46,7 +46,7 @@
 		{$field_value.value|escape}
 	{/if}
 	{foreach from=$field_value.categs item=categ name=fcategs}
-		{$categ.name|tr_if}
+		{$categ.categpath|tr_if}
 		{if !$smarty.foreach.fcategs.last}<br />{/if}
 	{/foreach}
 

commit 0803b9a0ffd738cdf9867e405cf25e2bd50efdba

[FIX] TRACKERFILTER: presumed breakage resulting from r29936 Note: untested, review welcome

lib/wiki-plugins/wikiplugin_trackerfilter.php
@@ -455,7 +455,7 @@ function wikiplugin_trackerFilter_get_filters($trackerId=0, $listfields='', &$fo
 			case 'e':// category
 				global $categlib; include_once('lib/categories/categlib.php');
 				if (ctype_digit($field['options_array'][0]) && $field['options_array'][0] > 0) {
-					if (isset($fopt['options_array'][3]) && $fopt['options_array'][3] == 1) {
+					if (isset($field['options_array'][3]) && $field['options_array'][3] == 1) {
 						$type = 'descendants';
 					} else {
 						$type = 'children';