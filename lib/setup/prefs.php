TheOnlyMerlin/Tiki

commit 98203ae90ff1dce87787bb338ab3534363c39fd8

[REF] initialize_prefs(): Optimize by caching the whole default preferences + unserialized modified preferences, as happened with the second-level preferences cache ($_SESSION["s_prefs"]) This new global_preferences cache replaces the previous modified_preferences. getModifiedPreferences() is almost only called by initialize_prefs()
git-svn-id: file:///var/local/code.tiki.org/tikisvn/trunk@38913 b456876b-0849-0410-b77d-98878d47e9d5

lib/setup/prefs.php
@@ -325,54 +325,61 @@ function get_default_prefs() {
 
 
 function initialize_prefs() {
-	global $prefs, $tikilib, $user_overrider_prefs, $in_installer, $section;
+	global $prefs, $tikilib, $user_overrider_prefs, $in_installer, $section, $systemConfiguration;
 
-	$defaults = get_default_prefs();
-
-	// Find which preferences need to be serialized/unserialized, based on the default values (those with arrays as values)
+	if (!empty($in_installer)) {
+			$prefs = get_default_prefs();
+			return;
+	}
 	$cachelib = TikiLib::lib('cache');
-	if ( !$cachelib->isCached('serialized_preferences')) {
+
+	if ($cachelib->isCached('global_preferences')) {
+		$prefs = $cachelib->getSerialized('global_preferences');
+	} else {
+		$defaults = get_default_prefs();
+	
+		// Find which preferences need to be serialized/unserialized, based on the default values (those with arrays as values)
 		$serializedPreferences = array();
 		foreach ( $defaults as $preference => $value ) {
 			if ( is_array($value) ) {
 				$serializedPreferences[] = $preference;
 			}
 		}
-		$cachelib->cacheItem('serialized_preferences', serialize($serializedPreferences));
-	}
-
-	$modified = empty($in_installer) ? $tikilib->getModifiedPreferences() : array();
-
-	// Unserialize serialized preferences
-	foreach ( $cachelib->getSerialized('serialized_preferences') as $serializedPreference ) {
-		if ( isset($modified[$serializedPreference]) && ! is_array($modified[$serializedPreference]) ) $modified[$serializedPreference] = @unserialize($modified[$serializedPreference]);
-	}
-
-	// Keep some useful sites values available before overriding with user prefs
-	// (they could be used in templates, so we need to set them even for Anonymous)
-	foreach ( $user_overrider_prefs as $uop ) {
-		if (isset($modified[$uop])) {
-			$modified['site_'.$uop] = $modified[$uop];
-		} elseif (isset($defaults[$uop])) {
-			$modified['site_'.$uop] = $defaults[$uop];
+	
+		$modified = $tikilib->getModifiedPreferences();
+	
+		// Unserialize serialized preferences
+		foreach ( $cachelib->getSerialized('serialized_preferences') as $serializedPreference ) {
+			if ( isset($modified[$serializedPreference]) && ! is_array($modified[$serializedPreference]) ) $modified[$serializedPreference] = @unserialize($modified[$serializedPreference]);
 		}
+	
+		// Keep some useful sites values available before overriding with user prefs
+		// (they could be used in templates, so we need to set them even for Anonymous)
+		foreach ( $user_overrider_prefs as $uop ) {
+			if (isset($modified[$uop])) {
+				$modified['site_'.$uop] = $modified[$uop];
+			} elseif (isset($defaults[$uop])) {
+				$modified['site_'.$uop] = $defaults[$uop];
+			}
+		}
+	
+		$prefs = $modified + $defaults;
+		$cachelib->cacheItem('global_preferences', serialize($prefs));
 	}
-
 
 	// Perspectives are disabled by default so the preference has to be modified
 	if ( isset($modified['feature_perspective']) && $modified['feature_perspective'] == 'y') {
 		if ( ! isset( $section ) || $section != 'admin' ) {
 			require_once 'lib/perspectivelib.php';
 			if ( $persp = $perspectivelib->get_current_perspective( $modified ) ) {
-				$changes = $perspectivelib->get_preferences( $persp );
-				$modified = array_merge( $modified, $changes );
+				$perspectivePreferences = $perspectivelib->get_preferences( $persp );
+				$prefs = $perspectivePreferences + $prefs;
 			}
 		}
 	}
 
-	global $systemConfiguration;
-	// Override default values with modified preferences, and override the result with system-configured preferences.
-	$prefs = $systemConfiguration->preference->toArray() + $modified + $defaults;
+	// Override preferences with system-configured preferences.
+	$prefs = $systemConfiguration->preference->toArray() + $prefs;
 }
 
 /**

lib/cache/cachelib.php
@@ -74,7 +74,7 @@ class Cachelib
 			$this->erase_dir_content("temp/cache/$tikidomain");
 			$this->erase_dir_content("modules/cache/$tikidomain");
 			$this->flush_opcode_cache();
-			$this->invalidate('modified_preferences');
+			$this->invalidate('global_preferences');
 			if (is_object($logslib)) {
 				$logslib->add_log( $log_section, 'erased all cache content');
 			}
@@ -105,7 +105,7 @@ class Cachelib
 			}
 		}
 		if (in_array( 'prefs', $dir_names)) {
-			$this->invalidate('modified_preferences');
+			$this->invalidate('global_preferences');
 		}
 	}
 

lib/core/Connect/Client.php
@@ -26,7 +26,7 @@ class Connect_Client extends Connect_Abstract
 
 		if ($prefs['connect_send_anonymous_info'] === 'y') {
 			$cachelib = TikiLib::lib('cache');
-			$cachelib->invalidate('modified_preferences');
+			$cachelib->invalidate('global_preferences');
 			
 			$tikilib = TikiLib::lib('tiki');
 			$modifiedPrefs = $tikilib->getModifiedPreferences();

lib/tikilib.php
@@ -3405,11 +3405,6 @@ class TikiLib extends TikiDb_Bridge
 	// NOTE: prefslib contains a similar method now called getModifiedPrefsForExport
 	function getModifiedPreferences()
 	{
-		$cachelib = TikiLib::lib('cache');
-		if ( $data = $cachelib->getSerialized('modified_preferences')) {
-			return $data;
-		}
-
 		$defaults = get_default_prefs();
 		$modified = array();
 
@@ -3422,9 +3417,6 @@ class TikiLib extends TikiDb_Bridge
 			if ( !isset($defaults[$name]) || (string) $defaults[$name] != (string) $value )
 				$modified[$name] = $value;
 		}
-
-		$cachelib->cacheItem('modified_preferences', serialize($modified));
-
 		return $modified;
 	}
 
@@ -3469,7 +3461,7 @@ class TikiLib extends TikiDb_Bridge
 	{
 		$this->table('tiki_preferences')->delete(array('name' => $name));
 		$cachelib = TikiLib::lib('cache');
-		$cachelib->invalidate('modified_preferences');
+		$cachelib->invalidate('global_preferences');
 	}
 
 	function set_preference($name, $value)
@@ -3488,7 +3480,7 @@ class TikiLib extends TikiDb_Bridge
 		$menulib->empty_menu_cache();
 
 		$cachelib = TikiLib::lib('cache');
-		$cachelib->invalidate('modified_preferences');
+		$cachelib->invalidate('global_preferences');
 
 		$preferences = $this->table('tiki_preferences');
 		$preferences->insertOrUpdate(array('value' => is_array($value) ? serialize($value) : $value), array('name' => $name));

commit 0dfb230a96fa20af648883c0b9539254bf477080

[FIX] Perspectives regression from r38913 (oops)

lib/setup/prefs.php
@@ -368,7 +368,7 @@ function initialize_prefs() {
 	if ( $prefs['feature_perspective'] == 'y') {
 		if ( ! isset( $section ) || $section != 'admin' ) {
 			require_once 'lib/perspectivelib.php';
-			if ( $persp = $perspectivelib->get_current_perspective( $modified ) ) {
+			if ( $persp = $perspectivelib->get_current_perspective( $prefs ) ) {
 				$perspectivePreferences = $perspectivelib->get_preferences( $persp );
 				$prefs = $perspectivePreferences + $prefs;
 			}