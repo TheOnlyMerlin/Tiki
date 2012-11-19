228341a74303b0f3ad00f44d4b82a3473898416f
robertplummer <robertplummer@b456876b-0849-0410-b77d-98878d47e9d5>
2012-11-19 20:57:46 +0000

    [MOD] Upgrade ckeditor to latest nightly 4 as of Nov 19th 2012

--- third_party/ckeditor/styles.js
+++ third_party/ckeditor/styles.js
@@ -3,12 +3,22 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
+// This file contains style definitions that can be used by CKEditor plugins.
+//
+// The most common use for it is the "stylescombo" plugin, which shows a combo
+// in the editor toolbar, containing all styles. Other plugins instead, like
+// the div plugin, use a subset of the styles on their feature.
+//
+// If you don't have plugins that depend on this file, you can simply ignore it.
+// Otherwise it is strongly recommended to customize this file to match your
+// website requirements and design properly.
+
 CKEDITOR.stylesSet.add( 'default', [
 	/* Block Styles */
 
-	// These styles are already available in the "Format" combo, so they are
-	// not needed here by default. You may enable them to avoid placing the
-	// "Format" combo in the toolbar, maintaining the same features.
+	// These styles are already available in the "Format" combo ("format" plugin),
+	// so they are not needed here by default. You may enable them to avoid
+	// placing the "Format" combo in the toolbar, maintaining the same features.
 	/*
 	{ name: 'Paragraph',		element: 'p' },
 	{ name: 'Heading 1',		element: 'h1' },
@@ -28,6 +38,7 @@ CKEDITOR.stylesSet.add( 'default', [
 
 	// These are core styles available as toolbar buttons. You may opt enabling
 	// some of them in the Styles combo, removing them from the toolbar.
+	// (This requires the "stylescombo" plugin)
 	/*
 	{ name: 'Strong',			element: 'strong', overrides: 'b' },
 	{ name: 'Emphasis',			element: 'em'	, overrides: 'i' },
