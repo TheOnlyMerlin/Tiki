TheOnlyMerlin/Tiki

commit 02d7803357944ed1e1720109ba5c92e8c0825653

[FIX] memcache: Flush memcache when flushing other caches
git-svn-id: file:///var/local/code.tiki.org/tikisvn/branches/11.x@46318 b456876b-0849-0410-b77d-98878d47e9d5

lib/cache/cachelib.php
@@ -81,6 +81,7 @@ class Cachelib
 			$this->erase_dir_content("temp/cache/$tikidomain");
 			$this->erase_dir_content("modules/cache/$tikidomain");
 			$this->flush_opcode_cache();
+			$prefs = $this->flush_memcache();
 			$this->invalidate('global_preferences');
 			if (is_object($logslib)) {
 				$logslib->add_log($log_section, 'erased all cache content');
@@ -188,6 +189,24 @@ class Cachelib
 				xcache_clear_cache(XC_TYPE_PHP, $index);
 			}
 		}
+	}
+
+	/**
+	 * Flush memcache if endabled
+	 *
+	 * @return void
+	 */
+	function flush_memcache()
+	{
+		global $prefs;
+
+		if (isset($prefs['memcache_enabled']) && $prefs['memcache_enabled'] == 'y') {
+			$memcachelib = TikiLib::lib("memcache");
+			if ($memcachelib->isEnabled()) {
+				$memcachelib->flush();
+			}
+		}
+		return;
 	}
 
 	function erase_dir_content($path)

commit 07ac2e8a95ae789c40727df05ac3b182b36dc975

[FIX] cache: Don't set $prefs to null when clearing all caches (regression from r46318, one of mine... and i'm mystified)

lib/cache/cachelib.php
@@ -96,7 +96,7 @@ class Cachelib
 			array_map('unlink', $banner);
 
 			$this->flush_opcode_cache();
-			$prefs = $this->flush_memcache();
+			$this->flush_memcache();
 			$this->invalidate('global_preferences');
 			if (is_object($logslib)) {
 				$logslib->add_log($log_section, 'erased all cache content');