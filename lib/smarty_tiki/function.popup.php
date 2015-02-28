TheOnlyMerlin/Tiki

commit 0e30df451b38a41373bd7cd48c13f5bee684e10d

[bp/r54086][FIX] Mouseover option no longer works in PluginImg (thanks Mani)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@54090 b456876b-0849-0410-b77d-98878d47e9d5

lib/smarty_tiki/function.popup.php
@@ -51,7 +51,6 @@ function smarty_function_popup($params, $smarty)
 						$options['data-trigger'] = 'click';
 						break;
 					default:
-						$options['data-trigger'] = $value;
 						break;
 				}
 				break;
@@ -63,7 +62,7 @@ function smarty_function_popup($params, $smarty)
 				$options[$key] = $value;
 				break;
 			case 'sticky':
-				//$options['data-trigger'] = 'focus'; // doesn't seem to work
+				$options['data-trigger'] = 'click';
 				break;
 			case 'fullhtml':
 				$options['data-html'] = true;

commit 4a36891ea1eedd6b08c72a2574927814374d6286

[MRG] branches/14.x 54138. Fixed Bug View Trackeritem https://dev.tiki.org/item5562 introduced in r54090 bp/r54086 (addressed bug in ImgPlugin). Also final fix for ImgPlugin. Updated doc for ImgPlugin https://doc.tiki.org/tiki-index.php?page_ref_id=4393#content_index2-2.

lib/smarty_tiki/function.popup.php
@@ -25,8 +25,9 @@
  * params still relevant:
  *
  *     text        Required: the text/html to display in the popup window
- *     trigger     'onMouseOver' or 'onClick' (onMouseOver default)
- *     sticky      false/true
+ *     trigger     'onClick' and native bootstrap params: 'click', 'hover', 'focus', 'manual' ('hover' default)
+ *     sticky      false/true - this is currently an alias for trigger['click'] which is wrong. 
+ *     							Sticky should define wether the popup should stay until clicked, not how it is triggered. 
  *     width       in pixels?
  *     fullhtml
  */
@@ -46,9 +47,17 @@ function smarty_function_popup($params, $smarty)
 				break;
 			case 'trigger':
 				switch ($value) {
+					// is this legacy? should not be used anywhere
 					case 'onclick':
 					case 'onClick':
 						$options['data-trigger'] = 'click';
+						break;
+					// support native bootstrap params - could be moved to default but not sure wether it breaks something
+					case 'click':
+					case 'hover':
+					case 'focus':
+					case 'manual':
+						$options['data-trigger'] = $value;
 						break;
 					default:
 						break;

lib/wiki-plugins/wikiplugin_img.php
@@ -1021,12 +1021,17 @@ function wikiplugin_img( $data, $params )
 			if ($imgdata['thumb'] == 'mousesticky') {
 				$popup_params['sticky'] = true;
 			}
-			$smarty->loadPlugin('smarty_function_popup');
 			
+			if ($imgdata['thumb'] == 'mouseover') {
+				$popup_params['trigger'] = 'hover';
+			} 
+			// avoid big images will not be closeable on hover. Fallback to require a click to open and a second click somewhere to close.
 			if ($fwidth > 400 || $fheight > 400) {
 				$popup_params['trigger'] = 'focus';
 			}
-
+			
+			$smarty->loadPlugin('smarty_function_popup');
+			
 			$mouseover = ' ' . smarty_function_popup($popup_params, $smarty);
 		} else {
 			if (!empty($imgdata['fileId']) && $imgdata['thumb'] != 'download' && empty($urldisp)) {

templates/trackeroutput/popup.tpl
@@ -7,4 +7,4 @@
 		</table>
 	</div>
 {/capture}
-{popup text=$smarty.capture.popup|escape:"javascript"|escape:"html" fullhtml="1" hauto=true vauto=true sticky=$stickypopup|default:null}
+{popup text=$smarty.capture.popup|escape:"javascript"|escape:"html" fullhtml="1" hauto=true vauto=true trigger="hover"}