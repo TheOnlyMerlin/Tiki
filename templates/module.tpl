TheOnlyMerlin/Tiki

commit 36d3e93a251c4dcfe00d1f655471430101bdbe85

[FIX] smarty: Restore tiki's own modifier.escape.php which stopped working several version ago (smarty 3 upgrade i think) and then fix up the parser, editpage etc to suit
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@56031 b456876b-0849-0410-b77d-98878d47e9d5

templates/module.tpl
@@ -13,7 +13,7 @@
         <div class="panel-heading">
             <h3 class="panel-title clearfix" {if !empty($module_params.bgcolor)} style="background-color:{$module_params.bgcolor};"{/if}>
                 {if $module_notitle ne 'y'}
-                    <span class="moduletitle">{$module_title}</span>
+                    <span class="moduletitle">{$module_title|escape}</span>
                 {/if}
                 {if $module_flip eq 'y' and $prefs.javascript_enabled ne 'n'}
                     <span class="moduleflip" id="moduleflip-{$smarty.capture.name}">

lib/init/smarty.php
@@ -166,6 +166,10 @@ class Smarty_Tiki extends Smarty
 		  require_once ('tiki-sefurl.php');
 		  $this->registerFilter('output', 'filter_out_sefurl');
 		}
+
+		// restore tiki's own escape function
+		$this->loadPlugin('smarty_modifier_escape');
+		$this->registerPlugin('modifier', 'escape', 'smarty_modifier_escape');
 	}
 
 	/**

lib/parser/parserlib.php
@@ -1556,11 +1556,6 @@ if ( \$('#$id') ) {
 
 		/* <x> XSS Sanitization handling */
 
-		// Converts &lt;x&gt; (<x> tag using HTML entities) into the tag <x>. This tag comes from the input sanitizer (XSS filter).
-		// This is not HTML valid and avoids using <x> in a wiki text,
-		//   but hide '<x>' text inside some words like 'style' that are considered as dangerous by the sanitizer.
-		$data = str_replace(array( '&lt;x&gt;', '~np~', '~/np~' ), array( '<x>', '~np~', '~/np~' ), $data);
-
 		// Fix false positive in wiki syntax
 		//   It can't be done in the sanitizer, that can't know if the input will be wiki parsed or not
 		$data = preg_replace('/(\{img [^\}]+li)<x>(nk[^\}]+\})/i', '\\1\\2', $data);
@@ -1706,6 +1701,11 @@ if ( \$('#$id') ) {
 
 		// Put removed strings back.
 		$this->replace_preparse($data, $preparsed, $noparsed, $this->option['is_html']);
+
+		// Converts &lt;x&gt; (<x> tag using HTML entities) into the tag <x>. This tag comes from the input sanitizer (XSS filter).
+		// This is not HTML valid and avoids using <x> in a wiki text,
+		//   but hide '<x>' text inside some words like 'style' that are considered as dangerous by the sanitizer.
+		$data = str_replace(array( '&lt;x&gt;', '~np~', '~/np~' ), array( '<x>', '~np~', '~/np~' ), $data);
 
 		// Process pos_handlers here
 		foreach ($this->pos_handlers as $handler) {

lib/smarty_tiki/block.title.php
@@ -33,6 +33,7 @@ function smarty_block_title($params, $content, $template, &$repeat)
 
 	$template->loadPlugin('smarty_function_icon');
 	$template->loadPlugin('smarty_modifier_sefurl');
+	$template->loadPlugin('smarty_modifier_escape');
 
 	if ( ! isset($params['help']) ) $params['help'] = '';
 	if ( ! isset($params['admpage']) ) $params['admpage'] = '';
@@ -59,13 +60,13 @@ function smarty_block_title($params, $content, $template, &$repeat)
 	}
 
 	$html = '<h1 class="pagetitle">';
-	$html .= '<a class="' . $class . '"' . $metadata . ' href="' . $params['url'] . '">' . htmlspecialchars($content) . "</a>\n";
+	$html .= '<a class="' . $class . '"' . $metadata . ' href="' . $params['url'] . '">' . smarty_modifier_escape($content) . "</a>\n";
 
 	if ($template->getTemplateVars('print_page') != 'y') {
 		if ( $prefs['feature_help'] == 'y' && $prefs['helpurl'] != '' && $params['help'] != '' ) {
 			$html .= '<a href="' ;
 
-			$html .= $prefs['helpurl'] . rawurlencode($params['help']) . '" class="tips btn btn-link" title="' . htmlspecialchars($content) . '|' . tra('Help page') . '" target="tikihelp">'
+			$html .= $prefs['helpurl'] . rawurlencode($params['help']) . '" class="tips btn btn-link" title="' . smarty_modifier_escape($content) . '|' . tra('Help page') . '" target="tikihelp">'
 			. smarty_function_icon(array('name' => 'help'), $template)
 			. "</a>\n";
 		}

lib/smarty_tiki/modifier.escape.php
@@ -26,18 +26,20 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
  * @link http://smarty.php.net/manual/en/language.modifier.escape.php
  *          escape (Smarty online manual)
  * @author   Monte Ohrt <monte at ohrt dot com>
- * @param string
  * @param html|htmlall|url|quotes|hex|hexentity|javascript
+ * @param string $esc_type
+ * @param string $char_set
+ * @param bool $double_encode
  * @return string
  */
-function smarty_modifier_escape($string, $esc_type = 'html', $char_set = 'UTF-8')
+function smarty_modifier_escape($string, $esc_type = 'html', $char_set = 'UTF-8', $double_encode = true)
 {
 	switch ($esc_type) {
 		case 'html':
 			if (is_array($string)) {
 				$string = implode(',', $string);
 			}
-			$return = htmlspecialchars($string, ENT_QUOTES, $char_set);
+			$return = htmlspecialchars($string, ENT_QUOTES, $char_set, $double_encode);
 			// Convert back sanitization tags into real tags to avoid them to be displayed
 			$return = str_replace('&lt;x&gt;', '<x>', $return);
 			// Convert back sanitization tags into real tags for no wrap space

lib/tikilib.php
@@ -4546,10 +4546,6 @@ class TikiLib extends TikiDb_Bridge
 			$html = 1;
 		}
 
-		if ( $html == 0 ) {
-			$edit_data = str_replace('<x>', '', $edit_data);
-		}
-
 		$parserlib = TikiLib::lib('parser');
 		$edit_data = $parserlib->process_save_plugins(
 			$edit_data,

tiki-editpage.php
@@ -840,7 +840,9 @@ if ( $prefs['wiki_authors_style_by_page'] === 'y' ) {
 if ($is_html) {
 	$smarty->assign('allowhtml', 'y');
 } else {
-	$edit_data = str_replace('<x>', '', $edit_data);
+	if (!empty($_REQUEST['preview'])) {
+		$edit_data = str_replace('<x>', '', $edit_data);
+	}
 	$smarty->assign('allowhtml', 'n');
 }
 if (empty($_REQUEST['lock_it']) && !empty($info['flag']) && $info['flag'] === 'L') {

commit 796feb4c440b0f9c5af8fead50e405997c56cea4

[FIX] modules: Don't html escape module titles as sometimes html needs to be used there (regression from r56031 - can't remember now why i added escape on module titles but this really needs to be done "upstream" from here)

templates/module.tpl
@@ -13,7 +13,7 @@
         <div class="panel-heading">
             <h3 class="panel-title clearfix" {if !empty($module_params.bgcolor)} style="background-color:{$module_params.bgcolor};"{/if}>
                 {if $module_notitle ne 'y'}
-                    <span class="moduletitle">{$module_title|escape}</span>
+                    <span class="moduletitle">{$module_title}</span>
                 {/if}
                 {if $module_flip eq 'y' and $prefs.javascript_enabled ne 'n'}
                     <span class="moduleflip" id="moduleflip-{$smarty.capture.name}">