640c6685a057a1678b343f7b9a1b251be5b5f406
nkoth <nkoth@b456876b-0849-0410-b77d-98878d47e9d5>
2016-02-02 16:27:29 +0000

    [REL] Tagging release

---
+++ tags/12.6/lib/wiki-plugins/wikiplugin_usercount.php
@@ -0,0 +1,43 @@
+<?php
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+function wikiplugin_usercount_info()
+{
+	return array(
+		'name' => tra('User Count'),
+		'documentation' => 'PluginUserCount',
+		'description' => tra('Display number of users for a site or group'),
+		'prefs' => array( 'wikiplugin_usercount' ),
+		'body' => tra('Group name'),
+		'icon' => 'img/icons/group_gear.png',
+		'params' => array(
+			'groups' => array(
+				'required' => false,
+				'name' => tra('Groups'),
+				'description' => tra('List of colon separated groups where a consolidated user count for multiple groups is needed.'),
+				'separator' => ':',
+				'default' => '',
+			),		
+		),
+	);
+}
+
+function wikiplugin_usercount($data, $params)
+{
+	global $tikilib, $userlib;
+
+	extract($params, EXTR_SKIP);
+
+	if ( isset( $params['groups'] ) ) {
+		$groups = $params['groups'];
+		$numusers = $userlib->count_users_consolidated($groups);
+	} else {
+		$numusers = $userlib->count_users($data);
+	}
+
+	return $numusers;
+}
